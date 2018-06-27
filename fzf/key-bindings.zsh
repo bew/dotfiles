d615d5657ee32f56a81cd35633f6e0c4691c32f5
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2018-06-27 01:53:06 -0700

    fzf: tabs to spaces (code)

--- fzf/key-bindings.zsh
+++ fzf/key-bindings.zsh
@@ -1,7 +1,7 @@
 # Key bindings
 # ------------
 if [[ $- != *i* ]]; then
-	return
+    return
 fi
 
 function __results_to_path_args
@@ -16,13 +16,13 @@ function __results_to_path_args
 
 function __fzfcmd
 {
-	echo "fzf ${FZF_BEW_LAYOUT} ${FZF_BEW_KEYBINDINGS}"
+    echo "fzf ${FZF_BEW_LAYOUT} ${FZF_BEW_KEYBINDINGS}"
 }
 
 function zwidget::fzf::find_file
 {
-	local completion_prefix=${LBUFFER/* /}
-	local lbuffer_without_completion_prefix="${LBUFFER%${completion_prefix}}"
+    local completion_prefix=${LBUFFER/* /}
+    local lbuffer_without_completion_prefix="${LBUFFER%${completion_prefix}}"
 
     local base_dir=${completion_prefix:-./}
     base_dir=${~base_dir} # expand ~ (at least)
@@ -32,30 +32,30 @@ function zwidget::fzf::find_file
     local cpl=$(cd ${base_dir}; "${finder_cmd[@]}" | "${fzf_cmd[@]}" | __results_to_path_args "$base_dir")
     local selected_completions=$cpl
 
-	if [ -n "$selected_completions" ]; then
-		LBUFFER="${lbuffer_without_completion_prefix}${selected_completions}"
-	fi
-	zle reset-prompt
+    if [ -n "$selected_completions" ]; then
+        LBUFFER="${lbuffer_without_completion_prefix}${selected_completions}"
+    fi
+    zle reset-prompt
 }
 zle -N zwidget::fzf::find_file
 
 function zwidget::fzf::find_directory
 {
-	local completion_prefix="${LBUFFER/* /}"
-	local lbuffer_without_completion_prefix="${LBUFFER%$completion_prefix}"
+    local completion_prefix="${LBUFFER/* /}"
+    local lbuffer_without_completion_prefix="${LBUFFER%$completion_prefix}"
 
     local base_dir=${completion_prefix:-./}
     base_dir=${~base_dir} # expand ~ (at least)
 
     local finder_cmd=(fd --type d)
     local fzf_cmd=($(__fzfcmd) --multi --prompt "$base_dir")
-	local cpl=$(cd $base_dir; "${finder_cmd[@]}" | "${fzf_cmd[@]}" | __results_to_path_args "$base_dir")
+    local cpl=$(cd $base_dir; "${finder_cmd[@]}" | "${fzf_cmd[@]}" | __results_to_path_args "$base_dir")
     local selected_completions=$cpl
 
-	if [ -n "$selected_completions" ]; then
-		LBUFFER="${lbuffer_without_completion_prefix}${selected_completions}"
-	fi
-	zle reset-prompt
+    if [ -n "$selected_completions" ]; then
+        LBUFFER="${lbuffer_without_completion_prefix}${selected_completions}"
+    fi
+    zle reset-prompt
 }
 zle -N zwidget::fzf::find_directory
 
@@ -70,14 +70,14 @@ function zwidget::fzf::history
     local history_cmd=(fc -lr 1)
 
     local fzf_cmd=($(__fzfcmd) $FZF_HISTORY_OPTIONS --query "${LBUFFER//$/\\$}")
-	local selected=( $( "${history_cmd[@]}" | "${fzf_cmd[@]}" ) )
-	if [ -n "$selected" ]; then
-		local history_index=$selected[1]
-		if [ -n "$history_index" ]; then
-			zle vi-fetch-history -n $history_index
-		fi
-	fi
-	zle reset-prompt
+    local selected=( $( "${history_cmd[@]}" | "${fzf_cmd[@]}" ) )
+    if [ -n "$selected" ]; then
+        local history_index=$selected[1]
+        if [ -n "$history_index" ]; then
+            zle vi-fetch-history -n $history_index
+        fi
+    fi
+    zle reset-prompt
 }
 zle -N zwidget::fzf::history
 
