d883112a25e90095fde20fa3702bc7877d987078
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-01-15 03:56:49 +0100

    Fix dl_fast_then_slow to end and handle sigint

--- bin/dl_fast_then_slow.sh
+++ bin/dl_fast_then_slow.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Some links can DL very fast (~3Mo/s) for a few seconds,
+# then go very slow for the rest of the time (~300ko/s)
+#
+# This script will launch the command, wait a few seconds,
+# then kill it and start again, bypassing the slow regulation :P
+
 if [[ $# == 0 ]]; then
     echo "Usage: $0 <program with args>"
     echo "(read the code for more infos)"
@@ -9,39 +15,49 @@ if [[ $# == 0 ]]; then
     exit 1
 fi
 
+PROC_PID=0
 
-# Some links can DL very fast (~3Mo/s) for a few seconds,
-# then go very slow for the rest of the time (~300ko/s)
-#
-# This script will launch the command, wait a few seconds,
-# then kill it and start again, bypassing the slow regulation :P
+function int_handler
+{
+    echo "Interrupted!"
+    if [[ "$PROC_PID" != 0 ]]; then
+        echo "Killing the underlying process to remove zombies.."
+        kill -KILL "$PROC_PID"
+    fi
+    exit 1
+}
+trap int_handler INT
 
 # An approximation of the time (in seconds) while the DL speed is fast
-FAST_TO_SLOW_DELAY=4
+FAST_TO_SLOW_DELAY=5
 
 # An approximation of the time (in seconds) the program takes to start
-PROGRAM_STARTUP_OVERHEAD=1
+PROGRAM_STARTUP_OVERHEAD=2
 
 delay=$(( FAST_TO_SLOW_DELAY + PROGRAM_STARTUP_OVERHEAD ))
 
 echo '-----------------------------------------------'
 
-# FIXME: currently the loop will never end
-# I don't know how to set `$ret` to the return status of the killed process..
-
-ret=1;
-until [[ $ret == 0 ]]; do
+still_processing=1;
+while [[ $still_processing == 1 ]]; do
     $@ &
-    pid=$!
+    PROC_PID=$!
 
-    echo "Started pid: $pid"
+    echo "Started pid: $PROC_PID"
     # wait for the program to start, and DL fast
     sleep $delay
 
-    kill -KILL $pid
-    sleep 1 # wait for it to stop
+    if ! kill -KILL "$PROC_PID"; then
+        # `kill` failed
+        echo "Failed to kill process $PROC_PID, assuming it is finished!"
+        still_processing=0
+    else
+        PROC_PID=0
+        # `kill` worked
+        sleep 1 # wait for it to stop
+    fi
 
-    echo # separator between invocations
+    echo # space separator between invocations
 done
 
 echo "Finished!"
