62f534f5befd1ee27268f7f29381e2c6996e3e4e
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2019-10-18 04:11:47 +0200

    Update crun

--- bin/crun_wrapper.sh
+++ bin/crun_wrapper.sh
@@ -8,7 +8,7 @@ function echo_err
 REPO_NAME='crun'
 REPO_PATH="$HOME/.bin/repos/$REPO_NAME"
 
-CRUN_BIN_PATH="$REPO_PATH/bin/crun"
+CRUN_BIN_PATH="$REPO_PATH/crun"
 
 if [[ -x "$CRUN_BIN_PATH" ]]; then
     if [[ -n "$CRUN_VERBOSE" ]]; then
