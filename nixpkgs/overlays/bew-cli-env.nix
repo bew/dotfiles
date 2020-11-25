fba59a67601e8e2fb1782bbf8baf8a1c01ab93d9
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2020-11-25 22:12:53 +0100

    nix: Convert packageOverrides to overlays <3

---
+++ nixpkgs/overlays/bew-cli-env.nix
@@ -0,0 +1,34 @@
+# This file must be in ~/.config/nixpkgs/overlays/
+# Ref: https://nixos.org/manual/nixpkgs/stable/#chap-overlays
+
+self: super:
+
+{
+  # Ref: https://nixos.org/nixpkgs/manual/#sec-declarative-package-management
+  bew-cli-env = super.buildEnv {
+    name = "bew-cli-env";
+    paths = let p = super; in [
+      p.neovim
+
+      p.fd
+      p.fzf
+      p.git
+      p.git-lfs
+      p.htop
+      p.jq
+      p.less
+      p.ripgrep
+      p.tmux
+      p.tree
+      p.zsh
+
+      p.cloc
+      p.httpie
+      p.ncdu
+      p.strace
+      p.watchexec
+    ] ++ [ # Add packages from my overlays
+      self.delta-bin
+    ];
+  };
+}
