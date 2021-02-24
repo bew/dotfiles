8b60072a0fa31e5157ce71f65f86e19029ba3931
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2021-02-24 12:36:41 +0100

    nix: Use final/prev in overlays

--- nixpkgs/overlays/bew-cli-env.nix
+++ nixpkgs/overlays/bew-cli-env.nix
@@ -1,34 +1,36 @@
 # This file must be in ~/.config/nixpkgs/overlays/
 # Ref: https://nixos.org/manual/nixpkgs/stable/#chap-overlays
 
-self: super:
+final: prev:
 
 {
   # Ref: https://nixos.org/nixpkgs/manual/#sec-declarative-package-management
-  bew-cli-env = super.buildEnv {
+  bew-cli-env = prev.buildEnv {
     name = "bew-cli-env";
-    paths = let p = super; in [
+    paths = let p = prev; in [
       p.neovim
 
       p.fd
       p.fzf
       p.git
       p.git-lfs
-      p.htop
       p.jq
-      # p.less # When enabled, less can't find the terminfo of my arch terminal (urxvt) :/
       p.ripgrep
-      p.tmux
       p.tree
       p.zsh
 
+      # When these are enabled, they can't find the terminfo of my arch terminal (urxvt) :/
+      # p.htop
+      # p.less
+      # p.tmux
+      # p.ncdu
+
       p.cloc
       p.httpie
-      p.ncdu
       p.strace
       p.watchexec
     ] ++ [ # Add packages from my overlays
-      self.delta-bin
+      final.delta-bin
     ];
   };
 }
