cc8158db9eb0f603d48083b52f6b7dffc6b8aef1
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2021-09-19 05:32:50 +0200

    wezterm: Use CTRL|SHIFT + low case letter!
    This was previously not possible to say:
        mods = "CTRL|SHIFT", key = "l"
    and we had to write:
        mods = "CTRL", key = "L"
    where it's harder to see that Ctrl+Shift is necessary.

--- gui/wezterm/cfg_keys.lua
+++ gui/wezterm/cfg_keys.lua
@@ -1,4 +1,5 @@
 local wezterm = require "wezterm"
+local act = wezterm.action
 
 function action_callback(event_id, func)
   wezterm.on(event_id, func)
@@ -9,31 +10,39 @@ local cfg = {}
 
 cfg.disable_default_key_bindings = true
 
--- NOTE: For bindings with mod SHIFT and a letter, the `key` field (the letter)
---       must be uppercase'd and the mods should NOT contain 'SHIFT'.
+-- NOTE: About SHIFT and the keybind definition:
+-- * For bindings with SHIFT and a letter, the `key` field (the letter)
+--   can be lowercase and the mods should NOT contain 'SHIFT'.
+-- * For bindings with SHIFT and something else, mod should contain SHIFT,
+--   and key should be the shifted key that is going to reach the terminal.
+--   (based on the keyboard-layout)
 cfg.keys = {
-  {mods = "SHIFT", key = "PageUp", action = wezterm.action{ScrollByPage = -1}},
-  {mods = "SHIFT", key = "PageDown", action = wezterm.action{ScrollByPage = 1}},
+  {mods = "SHIFT", key = "PageUp", action = act{ScrollByPage = -1}},
+  {mods = "SHIFT", key = "PageDown", action = act{ScrollByPage = 1}},
 
   -- Wezterm features
-  {mods = "CTRL", key = "R", action = "ReloadConfiguration"}, -- Ctrl-Shift-r
-  {mods = "CTRL", key = "L", action = wezterm.action{ClearScrollback = "ScrollbackAndViewport"}}, -- Ctrl-Shift-l
-  {mods = "CTRL", key = "F", action = wezterm.action{Search = {CaseInSensitiveString = ""}}}, -- Ctrl-Shift-f
+  {mods = "CTRL|SHIFT", key = "r", action = "ReloadConfiguration"},
+  {mods = "CTRL|SHIFT", key = "l", action = act{ClearScrollback = "ScrollbackAndViewport"}},
+  {mods = "CTRL|SHIFT", key = "f", action = act{Search = {CaseInSensitiveString = ""}}},
   {mods = "CTRL|SHIFT", key = " ", action = "QuickSelect"},
-  {mods = "CTRL", key = "D", action = "ShowDebugOverlay"},
+  {mods = "CTRL|ALT",   key = " ", action = "QuickSelect"}, -- note: eats a valid terminal keybind
+  {mods = "CTRL|SHIFT", key = "d", action = "ShowDebugOverlay"}, -- note: it's not a full Lua interpreter
 
-  -- Copy (to Clipboard) / Paste (from Clipboard or PrimarySelection)
-  {mods = "CTRL", key = "C", action = wezterm.action{CopyTo = "Clipboard"}}, -- Ctrl-Shift-c
-  {mods = "CTRL", key = "V", action = wezterm.action{PasteFrom = "Clipboard"}}, -- Ctrl-Shift-v
-  {mods = "SHIFT", key = "Insert", action = wezterm.action{PasteFrom = "PrimarySelection"}},
+  -- Copy/Paste to/from Clipboard
+  {mods = "CTRL|SHIFT", key = "c", action = act{CopyTo = "Clipboard"}},
+  {mods = "CTRL|SHIFT", key = "v", action = act{PasteFrom = "Clipboard"}},
+  -- Paste from PrimarySelection (Copy is done by selection)
+  {mods = "SHIFT",    key = "Insert", action = act{PasteFrom = "PrimarySelection"}},
+  {mods = "CTRL|ALT", key = "v",      action = act{PasteFrom = "PrimarySelection"}},
+  -- NOTE: the last one eats a valid terminal keybind
 
   -- Tabs
-  {mods = "CTRL",       key = "T", action = wezterm.action{SpawnTab="DefaultDomain"}}, -- Ctrl-Shift-t
-  {mods = "CTRL",       key = "Tab", action = wezterm.action{ActivateTabRelative=1}},
-  {mods = "CTRL|SHIFT", key = "Tab", action = wezterm.action{ActivateTabRelative=-1}},
-  {mods = "CTRL",       key = "W", action = wezterm.action{CloseCurrentTab={confirm=false}}}, -- Ctrl-Shift-w
+  {mods = "CTRL|SHIFT", key = "t", action = act{SpawnTab="DefaultDomain"}},
+  {mods = "CTRL",       key = "Tab", action = act{ActivateTabRelative=1}},
+  {mods = "CTRL|SHIFT", key = "Tab", action = act{ActivateTabRelative=-1}},
+  {mods = "CTRL|SHIFT", key = "w", action = act{CloseCurrentTab={confirm=false}}},
 
-  {mods = "CTRL", key = "X", action = "ShowLauncher"},
+  {mods = "CTRL|SHIFT", key = "x", action = "ShowLauncher"},
 
   -- Font size
   {mods = "CTRL", key = "0", action = "ResetFontSize"}, -- Ctrl-Shift-0
@@ -42,12 +51,11 @@ cfg.keys = {
 
   ---- custom events
 
-  -- Ctrl-Shift-g
-  {mods = "CTRL", key = "G", action = action_callback("my-toggle-ligature", function(win, _pane)
+  {mods = "CTRL|SHIFT", key = "g", action = action_callback("my-toggle-ligature", function(win, _)
     local overrides = win:get_config_overrides() or {}
     if not overrides.harfbuzz_features then
       -- If we haven't overriden it yet, then override with ligatures disabled
-      overrides.harfbuzz_features =  {"calt=0", "clig=0", "liga=0"}
+      overrides.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
     else
       -- else we did already, and we should disable the override now
       overrides.harfbuzz_features = nil
