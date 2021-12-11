19c75b0a8b4ea697acfaa2ea508cdfe700084c2f
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2021-12-11 06:29:26 +0100

    nix: Refactor home config, move out of flake.nix to make it configurable
    * flake inputs are declared in block
    * username is moved _out_ of the config, it's an external parameter now
    * pkgsChannels (with flakeInputs) are now passed as module args

--- flake.nix
+++ flake.nix
@@ -17,93 +17,27 @@
   # documented at https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-references
 
   # We use specific branches to get most/all packages from the official cache.
+  inputs = {
+    # This is the backbone package set, DO NOT REMOVE/CHANGE unless you know what you're doing
+    nixpkgsBackbone.url = "github:nixos/nixpkgs/nixos-21.11";
 
-  # This is the backbone package set, DO NOT REMOVE/CHANGE unless you know what you're doing
-  inputs.nixpkgsBackbone.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgsStable.url = "github:nixos/nixpkgs/nixos-21.11";
+    nixpkgsUnstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  inputs.nixpkgsStable.url = "github:nixos/nixpkgs/nixos-21.11";
-  inputs.nixpkgsUnstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flakeTemplates.url = "github:nixos/templates";
 
-  inputs.flakeTemplates.url = "github:nixos/templates";
-
-  inputs.homeManager = {
-    url = "github:nix-community/home-manager/release-21.11";
-    inputs.nixpkgs.follows = "nixpkgsStable";
+    homeManager.url = "github:nix-community/home-manager/release-21.11";
+    homeManager.inputs.nixpkgs.follows = "nixpkgsStable";
   };
 
   outputs = { self, ... }@inputs: {
     homeConfig = let
+      username = "lesell_b";
       # I only care about ONE system for now...
       system = "x86_64-linux";
     in import "${inputs.homeManager}/modules" {
       pkgs = inputs.nixpkgsStable.legacyPackages.${system};
-      configuration = { config, lib, ... }: {
-        imports = [
-          ./nix-home/modules/link-flake-inputs.nix
-          ./nix-home/modules/nix-registry.nix
-          ./nix-home/modules/pkgs-channels.nix
-          ./nix-home # my actual home package!
-        ];
-
-        # Extra module args
-        _module.args.flakeInputs = inputs;
-
-        # Link flake inputs to home
-        # => Easy way to search/read/navigate in any of them
-        linkFlakeInputs.directory = ".nix-home-current";
-
-        # Restrict user nix/registry.json to the following indirect flakes:
-        nixRegistry.indirectFlakes = {
-          # -> flake ref: "path:${nixpkgs.outPath}"
-          pkgs = { type = "path"; path = inputs.nixpkgsStable.outPath; };
-          # -> flake ref: "github:nixos/nixpkgs/nixpkgs-unstable"
-          unstable = {
-            type = "github";
-            owner = "nixos";
-            repo = "nixpkgs";
-            ref = "nixpkgs-unstable";
-          };
-          # -> flake ref: "path:${flakeTemplates.outPath}"
-          templates = { type = "path"; path = inputs.flakeTemplates.outPath; };
-        };
-
-        pkgsChannels = let
-          pkgsFromNixpkgs = nixpkgs: nixpkgs.legacyPackages.${system};
-        in {
-          backbone = pkgsFromNixpkgs inputs.nixpkgsBackbone;
-          stable = pkgsFromNixpkgs inputs.nixpkgsStable;
-          bleedingedge = pkgsFromNixpkgs inputs.nixpkgsUnstable;
-        };
-
-        home.username = "lesell_b";
-        # FIXME: this will be annoying to change to a dynamic DIR based on env for dev builds..
-        #   => Maybe mark them as the default?
-        #      (so we can override from a wrapper script or sth?)
-        #   => NOTE: using a switch with an env var like
-        #     `NIX_HOME_DEV_OVERRIDE` doesn't seem to trigger a rebuild..
-        home.homeDirectory = "/home/${config.home.username}";
-
-        # ---
-        # Proof of concept of a dynamic/editable symlink-to-a-dot-file, managed by Nix, using
-        # the `config.lib.file.mkOutOfStoreSymlink` helper function exposed by homeManager.
-        home.file.".test-out-of-store-gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dot/gitconfig";
-        # NOTE: (for when I finally commit and have my home dot-files managed by Nix)
-        # * https://github.com/nix-community/home-manager/issues/2085
-        # * https://www.reddit.com/r/NixOS/comments/qlhifw/comment/hj32l2b/ (my own comment on the subject)
-        home.file.".test-out-of-store-gitconfig--automatic".source = let
-          # Allows to give a ./relative/path/value and have it automatically converted
-          # to a path in my dotfiles directory. (Instead of being a path in the
-          # nix-stored flake source: /nix/store/...-source)
-          linkToDots = pathRaw: let
-            pathStr = toString pathRaw;
-            flakeSourceDir = toString self; # same as `toString ./.` # Could be computed elsewhere
-            actualDotsDir = "${config.home.homeDirectory}/.dot"; # Could be set in a custom module?
-
-            pathRelativeToDots = lib.removePrefix (flakeSourceDir + "/") pathStr;
-            pathAbsoluteToDots = "${actualDotsDir}/${pathRelativeToDots}";
-          in config.lib.file.mkOutOfStoreSymlink pathAbsoluteToDots;
-        in linkToDots ./gitconfig;
-      };
+      configuration = import ./nix-home { inherit inputs system username; };
     };
 
     # TODO(idea): expose packages (& apps?) of my tools pre-configured,
