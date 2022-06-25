2deab4edd1bf7d5774a3e806fbfe6a48400ae26c
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-06-25 21:05:31 +0200

    wezterm: Remove old unused font/configs & Update FontAwesome to v6

--- gui/wezterm/cfg_fonts.lua
+++ gui/wezterm/cfg_fonts.lua
@@ -4,7 +4,7 @@ local cfg = {}
 
 -- Disable annoying default behaviors
 cfg.adjust_window_size_when_changing_font_size = false
--- !! this one opens a separate win on first unknown glyph, stealing windows focus !!
+-- that one was opening a separate win on first unknown glyph, stealing windows focus (!!)
 cfg.warn_about_missing_glyphs = false
 
 cfg.font_size = 13.0
@@ -19,24 +19,8 @@ local function font_with_fallback(font_family)
   -- family names, not file names
   return wezterm.font_with_fallback({
     font_family,
-    "Font Awesome 5 Free Solid", -- nice double-spaced symbols!
+    "Font Awesome 6 Free Solid", -- nice double-spaced symbols!
   })
-end
-
-local function font_and_rules_for_iosevka()
-  -- Iosevka Font:
-  -- + Has 2 variants for terminals: Term & Fixed. Fixed is same as Term but without ligatures.
-  --   in the long run, I'd like to have a keybinding to enable/disable ligatures on demand,
-  --   by switching font for example.
-  --   --> for now, use Term (with ligatures)
-  --
-  -- + Has 2 additional variants for horizontal size: Normal & Extended. The Normal is the one
-  --   which does not mention 'Extended'. Extended is wider than Normal.
-  --   --> use Extended variant, the normal one is way too thin!!!
-  local font = font_with_fallback("Iosevka Term Extended")
-  local font_rules = nil
-  -- It finds automatically all the required fonts, no need to specify all the variants!
-  return font, font_rules
 end
 
 local function font_and_rules_for_jetbrains()
@@ -59,36 +43,12 @@ local function font_and_rules_for_jetbrains()
   return font, font_rules
 end
 
-local function font_and_rules_for_cascadia()
-  local font = font_with_fallback("Cascadia Code")
-  -- local font = font_with_fallback("Cascadia Code Light")
-  local font_rules = {
-    -- NOTE: There is no Italic in font Cascadia...
-    {
-      italic = true,
-      font = font_with_fallback("JetBrains Mono Italic"),
-    },
-    {
-      italic = true, intensity = "Bold",
-      font = font_with_fallback("JetBrains Mono Bold Italic"),
-    },
-    {
-      intensity = "Bold",
-      -- font = font_with_fallback("JetBrains Mono Bold"),
-      font = font_with_fallback("Cascadia Code Bold"),
-    },
-  }
-  return font, font_rules
-end
-
 -- FIXME (<- this is an example of bolded text)
 -- 0 1 2 3 4 5 6 7 8 9
 -- Some ligatures: != <-> <-  -> ----> => ==> ===> -- --- /../;;/ #{}
 --  <> <!-- --> ->> --> <= >= ++ == === := a::b::c a&&b a||b
 
 cfg.font, cfg.font_rules = font_and_rules_for_jetbrains()
--- cfg.font, cfg.font_rules = font_and_rules_for_cascadia()
--- cfg.font, cfg.font_rules = font_and_rules_for_iosevka()
 
 -- Enable various OpenType features
 -- See https://docs.microsoft.com/en-us/typography/opentype/spec/featurelist
