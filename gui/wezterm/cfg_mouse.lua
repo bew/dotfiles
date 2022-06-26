c652740c85617204ca84899f3a89cae06d2b9509
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-06-26 02:18:54 +0200

    wezterm: (udpate &) Rewrite config to adapt to new update \o/ !!!
    * Isolate all appearance in seperate file
    * Adapt all keybindings to use `wezterm.action.FooBar` action syntax <3
    * Nice 'mods' string generation helper (mods.SA => "SHIFT|ALT")
    * Rewrite mouse bindings helpers in a functional style <3
      (they're 'perfect' now!!! Short & composable :smiley:)
      (Add Block selection/extension!)
    * Setup a panes management key table (let' try!)

--- gui/wezterm/cfg_mouse.lua
+++ gui/wezterm/cfg_mouse.lua
@@ -1,5 +1,8 @@
 local wezterm = require "wezterm"
 local mytable = require "lib/mystdlib".mytable
+local mods = require "cfg_utils".mods
+
+local act = wezterm.action
 
 local cfg = {}
 
@@ -8,105 +11,104 @@ cfg.disable_default_mouse_bindings = true
 
 local mouse_bindings = {}
 
+local function binds_for_mouse_actions(mods, button, streak, mouse_actions)
+  local function mouse_bind_for(event_kind, action)
+    return {
+      mods=mods,
+      event = {
+        [event_kind] = {
+          streak = streak,
+          button = button
+        }
+      },
+      action = action,
+    }
+  end
 
--- Left click always starts a new selection.
--- The number of clicks determines the selection mode: 1:Cell 2:Word: 3:Line
-function binds_for_mouse_select(button, streak, selection_mode)
+  local binds = {}
+  if mouse_actions.down_action then
+    table.insert(binds, mouse_bind_for("Down", mouse_actions.down_action))
+  end
+  if mouse_actions.drag_action then
+    table.insert(binds, mouse_bind_for("Drag", mouse_actions.drag_action))
+  end
+  if mouse_actions.up_action then
+    table.insert(binds, mouse_bind_for("Up", mouse_actions.up_action))
+  end
+  return binds
+end
+
+local function initial_selection_mouse_actions(selection_mode)
   return {
     -- Select on Down event
-    {
-      mods="NONE",
-      event={Down={streak=streak, button=button}},
-      action=wezterm.action{SelectTextAtMouseCursor=selection_mode},
-    },
-
+    down_action = act.SelectTextAtMouseCursor(selection_mode),
     -- Extend on Drag event
-    {
-      mods="NONE",
-      event={Drag={streak=streak, button=button}},
-      action=wezterm.action{ExtendSelectionToMouseCursor=selection_mode},
-    },
-
-    -- Complete on Up event
-    {
-      mods="NONE",
-      event={Up={streak=streak, button=button}},
-      action=wezterm.action{CompleteSelection="PrimarySelection"}
-    },
+    drag_action = act.ExtendSelectionToMouseCursor(selection_mode),
+    -- Complete & Copy on Up event
+    up_action   = act.CompleteSelection("PrimarySelection"),
   }
 end
+-- Left click always starts a new selection.
+-- The number of clicks determines the selection mode: 1:Cell 2:Word: 3:Line & Alt+1:Block
 table.insert(mouse_bindings, {
-  binds_for_mouse_select("Left", 1, "Cell"),
-  binds_for_mouse_select("Left", 2, "Word"),
-  binds_for_mouse_select("Left", 3, "Line"),
+  binds_for_mouse_actions(mods._, "Left", 1, initial_selection_mouse_actions("Cell")),
+  binds_for_mouse_actions(mods._, "Left", 2, initial_selection_mouse_actions("Word")),
+  binds_for_mouse_actions(mods._, "Left", 3, initial_selection_mouse_actions("Line")),
+  binds_for_mouse_actions(mods.A, "Left", 1, initial_selection_mouse_actions("Block")),
 })
 
--- Right click always extends the selection.
--- The number of clicks determines the selection mode: 1:Cell 2:Word: 3:Line
-function binds_extend_mouse_select(button, streak, selection_mode)
+
+local function extend_selection_mouse_actions(selection_mode)
   return {
     -- Extend the selection on Down & Drag events
-    {
-      mods="NONE",
-      event={Down={streak=streak, button=button}},
-      action=wezterm.action{ExtendSelectionToMouseCursor=selection_mode},
-    },
-    {
-      mods="NONE",
-      event={Drag={streak=streak, button=button}},
-      action=wezterm.action{ExtendSelectionToMouseCursor=selection_mode},
-    },
-
-    -- Complete on Up event
-    {
-      mods="NONE",
-      event={Up={streak=streak, button=button}},
-      action=wezterm.action{CompleteSelection="PrimarySelection"}
-    },
+    down_action = act.ExtendSelectionToMouseCursor(selection_mode),
+    drag_action = act.ExtendSelectionToMouseCursor(selection_mode),
+    -- Complete & Copy on Up event
+    up_action   = act.CompleteSelection("PrimarySelection"),
   }
 end
+-- Right click always extends the selection.
+-- The number of clicks determines the selection mode: 1:Cell 2:Word: 3:Line & Alt+1:Block
 table.insert(mouse_bindings, {
-  binds_extend_mouse_select("Right", 1, "Cell"),
-  binds_extend_mouse_select("Right", 2, "Word"),
-  binds_extend_mouse_select("Right", 3, "Line"),
+  binds_for_mouse_actions(mods._, "Right", 1, extend_selection_mouse_actions("Cell")),
+  binds_for_mouse_actions(mods._, "Right", 2, extend_selection_mouse_actions("Word")),
+  binds_for_mouse_actions(mods._, "Right", 3, extend_selection_mouse_actions("Line")),
+  binds_for_mouse_actions(mods.A, "Right", 1, extend_selection_mouse_actions("Block")),
 })
 
 -- Ctrl-Left click (on Up) opens the link under the mouse pointer if any.
 -- (on Down, the click is disabled. This is to avoid bugging the running
 -- program which would receive _only_ the down event and not the up event)
 table.insert(mouse_bindings, {
-  mods="CTRL",
-  event={Down={streak=1, button="Left"}},
-  action="Nop",
-})
-table.insert(mouse_bindings, {
-  mods="CTRL",
-  event={Up={streak=1, button="Left"}},
-  action="OpenLinkAtMouseCursor",
+  binds_for_mouse_actions(mods.C, "Left", 1, {
+    down_action = act.Nop,
+    up_action   = act.OpenLinkAtMouseCursor,
+  }),
 })
 -- FIXME: I want this to work EVEN IF the current program enabled mouse-reporting.
 --        Currently I have to press Ctrl-Shift-click to make this binding work.
--- Another binding override I'd love is the MiddleClick (to work even in tmux!)
+-- Opened issue: https://github.com/wez/wezterm/issues/581
 
 -- Clipboard / PrimarySelection paste
 table.insert(mouse_bindings, {
   -- Middle click pastes from the primary selection (for any other mods).
   wezterm.permute_any_or_no_mods({
+    mods=mods._,
     event={Down={streak=1, button="Middle"}},
-    action=wezterm.action{PasteFrom = "PrimarySelection"},
+    action=act.PasteFrom("PrimarySelection"),
   }),
   -- Alt-Middle click pastes from the clipboard selection
   -- NOTE: Must be last to overwrite the existing Alt-Middle binding done by permute_any_or_no_mods.
   {
-    mods="ALT",
+    mods=mods.A,
     event={Down={streak=1, button="Middle"}},
-    action=wezterm.action{PasteFrom = "Clipboard"},
+    action=act.PasteFrom("Clipboard"),
   },
 })
 
-  -- To simplify config composability, `mouse_bindings` is a
-  -- nested list of (bind or list of (bind or ...)), so we must
-  -- flatten the list first.
+-- To simplify config composability, `mouse_bindings` is a
+-- nested list of (bind or list of (bind or ...)), so we must
+-- flatten the list first.
 cfg.mouse_bindings = mytable.flatten_list(mouse_bindings)
 
 return cfg
