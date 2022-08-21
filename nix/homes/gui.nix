5dbe06136d6e9eb48f14a9666ab67ef3172542d2
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-08-21 14:07:03 +0200

    gui,nix: Use latest copyq version :)

--- nix/homes/gui.nix
+++ nix/homes/gui.nix
@@ -2,6 +2,16 @@
 
 let
   inherit (pkgsChannels) stable bleedingedge;
+
+  copyq-latest = stable.copyq.overrideAttrs (oldAttrs: rec {
+    version = "6.2.0";
+    src = stable.fetchFromGitHub {
+      owner = "hluk";
+      repo = "CopyQ";
+      rev = "v${version}";
+      hash = "sha256-0XCqSF1oc2B3FD5OmOxqgt9sqCIrzK0KjkntVFXlRWI=";
+    };
+  });
 in {
   imports = [
     ./gui-fix-xdg-data-dirs.nix
@@ -12,7 +22,7 @@ in {
     stable.polybar
     stable.dunst
     # FIXME: herbstluftwm is missing here (can't get it to compile last version)
-    stable.copyq # powerful clipboard manager
+    copyq-latest # powerful clipboard manager
 
     stable.arandr
     stable.autorandr
