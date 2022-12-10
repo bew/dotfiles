10fc3e57c0d7ff51373c6a31e3161d7f46a29f40
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-12-10 15:31:04 +0100

    bin,nix: Rename some nix tools

--- nix/homes/cli.nix
+++ nix/homes/cli.nix
@@ -159,8 +159,12 @@ in {
     ])
 
     # Nix tools
-    stable.nix-tree # TUI to browse the dependencies of a derivation (https://github.com/utdemir/nix-tree)
-    stable.nix-diff # CLI to explain why 2 derivations differ (https://github.com/Gabriel439/nix-diff)
+    (mybuilders.linkBins "some-nix-tools" {
+      # TUI to browse the dependencies of a derivation (https://github.com/utdemir/nix-tree)
+      nix-explore = "${stable.nix-tree}/bin/nix-tree";
+      # CLI to explain why 2 derivations differ (https://github.com/Gabriel439/nix-diff)
+      nix-diff-drv = "${stable.nix-diff}/bin/nix-diff";
+    })
     stable.nixfmt # a Nix formatter (more at https://github.com/nix-community/nixpkgs-fmt#formatters)
     # stable.nix-update # Swiss-knife for updating nix packages
     # TODO: add nix-index!
