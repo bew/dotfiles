1a6063d5915a314983a42dc033d530be94f81a53
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-12-10 01:09:12 +0100

    repo: Update install_bruteforce script to be more robust
    - Allow dots repo to be anywhere
    - Give more info about what links are needed / replaced, and what it was
      before
    - add helper to skip an install and showing it in logs
    - skip nvim & zsh configs, they are setup by Nix now

---
+++ install_bruteforce.bash
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+function make-link
+{
+  local to="$1"
+  local from="$2"
+
+  local old_to="$(readlink "$from" || true)"
+  if [[ "$to" == "$old_to" ]]; then
+    echo "nothing to do, '$from' already points to '$to'"
+    return
+  fi
+
+  if [[ "$DRYRUN_ONLY" == "true" ]]; then
+    local before before_short
+    if [[ -n "$old_to" ]]; then
+      before_short=R
+      before="was '$old_to'"
+    else
+      before_short=N
+      before="to create"
+    fi
+    echo "DRYRUN[$before_short]: '$from' -> '$to' ($before)"
+  else
+    ln -vsf --no-dereference "$to" "$from"
+  fi
+}
+
+function label
+{
+  echo
+  echo "---- $1"
+}
+
+function skip
+{
+  echo "SKIPPED: $*"
+}
+
+DRYRUN_ONLY="false"
+if [[ "$1" == "--dry-run" ]]; then
+  DRYRUN_ONLY="true"
+fi
+DOTS_PATH=$(dirname $(realpath "$0"))
+
+label "Bootstraping .dot symlinks"
+make-link "$DOTS_PATH"  ~/.dot
+
+label "bin links"
+make-link ~/.dot/bin     ~/.bin
+make-link ~/.dot/gui/bin ~/.bin-gui
+
+label "zsh links"
+skip make-link ~/.dot/zsh/zshrc   ~/.zshrc
+skip make-link ~/.dot/zsh/zshenv  ~/.zshenv
+make-link ~/.dot/zsh/zlogin  ~/.zlogin
+
+label "nvim link"
+skip make-link ~/.dot/nvim  ~/.config/nvim
+
+label "git links"
+make-link ~/.dot/git  ~/.config/git
+make-link ~/.dot/gh   ~/.config/gh
+
+label "Nix stuff"
+make-link ~/.dot/nix/nix-self-config  ~/.config/nix
+
+label "other cli tools"
+make-link ~/.dot/htop            ~/.config/htop
+make-link ~/.dot/tmux/tmux.conf  ~/.tmux.conf
+make-link ~/.dot/gdb             ~/.config/gdb
+
+label "X configs"
+make-link ~/.dot/gui/xinitrc       ~/.xinitrc
+make-link ~/.dot/gui/xprofile      ~/.xprofile
+make-link ~/.dot/gui/xkbmap.config ~/.config/xkbmap.config
+make-link ~/.dot/gui/picom.config  ~/.config/picom.config # compositor
+make-link ~/.dot/gui/mimeapps.list ~/.config/mimeapps.list # xdg MIME type to apps associations
+make-link ~/.dot/gui/autorandr     ~/.config/autorandr
+
+label "X apps configs"
+# terms
+make-link ~/.dot/gui/wezterm   ~/.config/wezterm
+
+# Desktop env
+make-link ~/.dot/gui/herbstluftwm  ~/.config/herbstluftwm
+make-link ~/.dot/gui/polybar       ~/.config/polybar
+make-link ~/.dot/gui/dunst         ~/.config/dunst
+make-link ~/.dot/gui/flameshot     ~/.config/flameshot
+make-link ~/.dot/gui/copyq         ~/.config/copyq
+make-link ~/.dot/gui/libinput-gestures.conf  ~/.config/libinput-gestures.conf
+
+# tools
+make-link ~/.dot/gui/mpv       ~/.config/mpv
+make-link ~/.dot/gui/tridactyl ~/.config/tridactyl
+
+# vscode (if I ever want to have it..)
+if [[ -d ~/.config/"Code - OSS" ]]; then
+  make-link ~/.dot/gui/vscode-user-config ~/.config/"Code - OSS"/User
+fi
+
+if [[ "$DRYRUN_ONLY" == "true" ]]; then
+  echo
+  echo "### To apply the above config, re-run without --dry-run"
+  echo
+fi
