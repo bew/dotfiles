9d3556bd7c7a0c8921e446952be83f2bcd957dc2
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-12-15 07:49:08 +0100

    nix: Add builder to replace bins in a pkg while keeping its structure <3

--- nix/homes/mylib/mybuilders.nix
+++ nix/homes/mylib/mybuilders.nix
@@ -10,6 +10,12 @@ rec {
   #   in the same derivation or the same top-level environment (which is also a derivation).
   # * to expose only a few binaries of a derivation.
   #
+  # NOTE: Using linkBins, the packages used to define the new binaries can't install their normal
+  # outputs, thus 'man' output is never available... Similarly shell completions, icons, libs,
+  # includes, configs are not included.
+  # This builder is ONLY useful to make binaries available.
+  # => Use `replaceBinsInPkg` to replace bins in an existing package while keeping its structure.
+  #
   # Types:
   #   linkBins :: { name :: String; ... } -> derivation
   #   linkBins :: [ String or { name :: String; path :: String; } ] -> derivation
@@ -21,9 +27,9 @@ rec {
   #   ])
   #   => creates a derivation like:
   #     /nix/store/znk3qkb30ccgq6kvgmv69jj4ci9bin18-my-bins1/
-  #     `-- bin/
-  #         |-- nvim -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
-  #         `-- bar -> /tmp/foo/bar
+  #     └── bin/
+  #         ├── nvim -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
+  #         └── bar -> /tmp/foo/bar
   #
   #   (linkBins "my-bins2" [
   #     {name = "nvim-stable"; path = "${pkgs.neovim}/bin/nvim";}
@@ -31,9 +37,9 @@ rec {
   #   ])
   #   => creates a derivation like:
   #     /nix/store/f35182nny6lb95srh0lbxfd5hq99kr8s-my-bins2/
-  #     `-- bin/
-  #         |-- nvim-stable -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
-  #         `-- bar -> /tmp/foo/bar
+  #     └── bin/
+  #         ├── nvim-stable -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
+  #         └── bar -> /tmp/foo/bar
   #
   #   (linkBins "my-bins3" {
   #     nvim-stable = "${pkgs.neovim}/bin/nvim";
@@ -41,9 +47,9 @@ rec {
   #   })
   #   => creates a derivation like:
   #     /nix/store/3v6mk91b4n4758zli921y2z27xm3a5v2-my-bins3/
-  #     `-- bin/
-  #         |-- nvim-stable -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
-  #         `-- bar-tmp -> /tmp/foo/bar
+  #     └── bin/
+  #         ├── nvim-stable -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
+  #         └── bar-tmp -> /tmp/foo/bar
   #
   linkBins = name: binsSpec:
     let
@@ -81,7 +87,6 @@ rec {
             ${binSpecHelp}
           ''
       );
-
     in pkgs.runCommandLocal name {} ''
       mkdir -p $out/bin
       cd $out/bin
@@ -98,12 +103,85 @@ rec {
   #   linkSingleBin "${pkgs.neovim}/bin/nvim"
   #   => creates a derivation like:
   #     /nix/store/yv5aigjy8l9bi9kpqh7y1dzf6nv07cl0-nvim-single-bin/
-  #     `-- bin/
-  #         `-- nvim -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
+  #     └── bin/
+  #         └── nvim -> /nix/store/frlxim9yz5qx34ap3iaf55caawgdqkip-neovim-0.5.1/bin/nvim
   #
   linkSingleBin = path:
     pkgs.runCommandLocal "${baseNameOf path}-single-bin" { } ''
       mkdir -p $out/bin
       ln -s ${lib.escapeShellArg path} $out/bin/
     '';
+
+  # Copy full package, remove existing bin/ and replace with the given list of bins.
+  #
+  # This is useful to patch some binaries / force them to use some
+  # configuration, while preserving the pkg layout, allowing `man tool` to
+  # auto-find the manpage of the tool when it's installed in home.packages!
+  #
+  # Example:
+  #   replaceBinsInPkg {
+  #     name = "custom-fzf";
+  #     copyFromPkg = fzf;
+  #     bins = {
+  #       fzf = writeShellScript "fzf" ''
+  #         exec ${fzf}/bin/fzf --custom-args right here --and=here "$@"
+  #       '';
+  #     };
+  #     postBuild = "
+  #   }
+  #   => creates a derivation like:
+  #     /nix/store/xx3yknpx0yvnks0dqvsd11n7p1zm5mb4-custom-fzf
+  #     ├── bin
+  #     │   └── fzf
+  #     └── share
+  #         ├── fish -> /nix/store/3fz694nbl7ndyinz7xmhz77inzn0a17h-fzf-0.35.1/share/fish
+  #         ├── fzf -> /nix/store/3fz694nbl7ndyinz7xmhz77inzn0a17h-fzf-0.35.1/share/fzf
+  #         ├── man -> /nix/store/5fl47ah7k40j6pk0ln74wf3kby2h8jp1-fzf-0.35.1-man/share/man
+  #         └── vim-plugins -> /nix/store/3fz694nbl7ndyinz7xmhz77inzn0a17h-fzf-0.35.1/share/vim-plugins
+  #     
+  #   replaceBinsInPkg {
+  #     name = "custom-zsh";
+  #     copyFromPkg = zsh;
+  #     nativeBuildInputs = [ makeWrapper ];
+  #     postBuild = /* sh */ ''
+  #       makeWrapper ${zsh}/bin/zsh $out/bin/zsh --set ZDOTDIR /some/new/zdotdir
+  #     '';
+  #   };
+  #   => creates a derivation like:
+  #     /nix/store/p85wi35yda68xw9xr2s1lamgv4hqh1jl-custom-zsh
+  #     ├── bin
+  #     │   └── zsh
+  #     ├── etc -> /nix/store/dy11j8bd6a6gq0nsgx54zddg32qrcd7l-zsh-5.8.1/etc
+  #     ├── lib -> /nix/store/dy11j8bd6a6gq0nsgx54zddg32qrcd7l-zsh-5.8.1/lib
+  #     └── share -> /nix/store/dy11j8bd6a6gq0nsgx54zddg32qrcd7l-zsh-5.8.1/share
+  #
+  replaceBinsInPkg = { name, copyFromPkg, bins ? {}, nativeBuildInputs ? [], postBuild ? "" }:
+    pkgs.buildEnv {
+      inherit name nativeBuildInputs;
+      paths = [ copyFromPkg ];
+      postBuild = /* sh */ ''
+        if [[ -e $out/bin ]]; then
+          echo "Remove existing bin/ (was: `readlink $out/bin`)"
+          # No need for '-r', it's a symlink!
+          rm -f $out/bin
+        fi
+        echo "Create empty bin/"
+        mkdir $out/bin
+
+        ${lib.optionalString (0 != (lib.length (lib.attrNames bins))) ''
+          echo "Add binaries: ${lib.concatStringsSep ", " (lib.attrNames bins)}"
+          ${lib.concatStringsSep "\n"
+            (lib.mapAttrsToList
+              (name: targetBin: "cp ${toString targetBin} $out/bin/${name}")
+              bins
+            )
+          }
+        ''}
+
+        ${lib.optionalString (0 != (lib.stringLength postBuild)) ''
+          echo "Run postBuild to add more binaries"
+          ${postBuild}
+        ''}
+      '';
+    };
 }
