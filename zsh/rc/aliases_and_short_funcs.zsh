1e38327bf7c0a3faa6828d18b3aeeaeb73e40c5e
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2022-12-26 02:16:00 +0100

    bin: Add helper to show chain of symlinks to bin|path (useful with Nix!)

--- zsh/rc/aliases_and_short_funcs.zsh
+++ zsh/rc/aliases_and_short_funcs.zsh
@@ -183,7 +183,7 @@ alias todo='rg -i "todo|fixme" --colors=match:fg:yellow --colors=match:style:bol
 # Search the given bin name in PATH (ignores aliases/functions)
 # -p : always search in PATH
 # -s : show the realpath in addition to the path in PATH
-alias which::in-path='which -ps'
+alias which::realpath="echo \"-- note: use 'show-symlinks-chain' for more details\"; which -ps"
 
 # Always expose a known TERM (not the 256color version) to the server I'm connecting to.
 alias ssh='TERM=xterm ssh'
