5f98a0d5ce49979569d4a30251cae6d4a125e3a7
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2023-07-30 21:56:25 +0200

    nix,bin: Add @mpv helper script, nix-ified, packaged
    --- Closure diff:
    @mpv: ∅ → ε
    jo: ∅ → 1.9, +44.5 KiB
    mpv: ∅ → 0.35.0, +6494.8 KiB
    socat: ∅ → 1.7.4.3, +690.2 KiB
    freefont-ttf: ∅ → 20120503, +10547.4 KiB
    libXpresent: ∅ → 1.0.0, +26.7 KiB
    libbluray: ∅ → 1.3.2, +470.8 KiB
    libdvdcss: ∅ → 1.4.3, +195.1 KiB
    libdvdnav: ∅ → 6.1.1, +176.6 KiB
    libdvdread: ∅ → 6.1.3, +328.7 KiB
    libplacebo: ∅ → 4.208.0, +1125.4 KiB
    lua: ∅ → 5.2.4, +1446.6 KiB
    openal-soft: ∅ → 1.22.2, +1551.2 KiB
    rubberband: ∅ → 3.1.0, +2819.1 KiB
    shaderc: ∅ → 2022.4, +10975.2 KiB
    uchardet: ∅ → 0.0.7, +588.8 KiB
    vamp-plugin-sdk: ∅ → 2.10, +1835.7 KiB

---
+++ nix/pkgs/mpv-helpers/default.nix
@@ -0,0 +1,18 @@
+{
+  runCommandLocal,
+
+  jo,
+  socat,
+  mpv-unwrapped,
+  jq,
+}:
+
+runCommandLocal "@mpv" {} /* sh */ ''
+  mkdir -p $out/bin
+  substitute ${./mpv-helpers.sh} $out/bin/@mpv \
+    --replace "_BIN_mpv=" "_BIN_mpv=${mpv-unwrapped}/bin/mpv #" \
+    --replace "_BIN_socat=" "_BIN_socat=${socat}/bin/socat #" \
+    --replace "_BIN_jq=" "_BIN_jq=${jq}/bin/jq #" \
+    --replace "_BIN_jo=" "_BIN_jo=${jo}/bin/jo #"
+  chmod +x $out/bin/@mpv
+''
