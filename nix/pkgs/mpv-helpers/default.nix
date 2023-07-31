d8daa97bb36cf57ec264f2fec184b75560d08bac
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2023-07-31 18:06:14 +0200

    nix,bin: Fix @mpv helper to use my custom mpv with config
    I actually need it so that starting mpv using `@mpv daemon-start foo`
    will use my mpv with config!

--- nix/pkgs/mpv-helpers/default.nix
+++ nix/pkgs/mpv-helpers/default.nix
@@ -3,14 +3,14 @@
 
   jo,
   socat,
-  mpv-unwrapped,
+  mpv,
   jq,
 }:
 
 runCommandLocal "@mpv" {} /* sh */ ''
   mkdir -p $out/bin
   substitute ${./mpv-helpers.sh} $out/bin/@mpv \
-    --replace "_BIN_mpv=" "_BIN_mpv=${mpv-unwrapped}/bin/mpv #" \
+    --replace "_BIN_mpv=" "_BIN_mpv=${mpv}/bin/mpv #" \
     --replace "_BIN_socat=" "_BIN_socat=${socat}/bin/socat #" \
     --replace "_BIN_jq=" "_BIN_jq=${jq}/bin/jq #" \
     --replace "_BIN_jo=" "_BIN_jo=${jo}/bin/jo #"
