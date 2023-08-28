241021c76256a59be70a1744d155f0acd931d0c6
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2023-08-28 14:13:16 +0200

    nix: Add module for `dyndots.mkLink` to make editable/? links to dots

--- nix/homes/main.nix
+++ nix/homes/main.nix
@@ -10,6 +10,7 @@
   imports = [
     ./modules/link-flake-inputs.nix
     ./modules/nix-registry.nix
+    ./modules/dyndots.nix
     # Make (force) this home config install using `nix profile`, not `nix-env`
     ./modules/force-homemanager-to-use-nix-profiles.nix
   ] ++ [
@@ -46,6 +47,13 @@
   # Mark it as default, so I'll be able to override it for dev builds :)
   home.homeDirectory = lib.mkDefault "/home/${config.home.username}";
 
+  # Configure my dotfiles path, so that direct links created with `config.dyndots.mkLink` point to
+  # my repo (editable!).
+  dyndots.mode = "editable";
+  dyndots.dotfilesRepoPath = "${config.home.homeDirectory}/.dot";
+  dyndots.flakeStorePath = inputs.self;
+  home.file.".test-dyndots-link".source = config.dyndots.mkLink ../../git/config;
+
   # Link flake inputs to home
   # => Easy way to search/read/navigate in any of them
   linkFlakeInputs.directory = ".nix-home-current";
@@ -64,28 +72,6 @@
     # -> flake ref: "path:${flakeTemplates.outPath}"
     templates = { type = "path"; path = inputs.flakeTemplates.outPath; };
   };
-
-  # --------
-  # Proof of concept of a dynamic/editable symlink-to-a-dot-file, managed by Nix, using
-  # the `config.lib.file.mkOutOfStoreSymlink` helper function exposed by homeManager.
-  home.file.".test-out-of-store-gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dot/git/config";
-  # NOTE: (for when I finally commit and have my home dot-files managed by Nix)
-  # * https://github.com/nix-community/home-manager/issues/2085
-  # * https://www.reddit.com/r/NixOS/comments/qlhifw/comment/hj32l2b/ (my own comment on the subject)
-  home.file.".test-out-of-store-gitconfig--automatic".source = let
-    # Allows to give a ./relative/path/value and have it automatically converted
-    # to a path in my dotfiles directory. (Instead of being a path in the
-    # nix-stored flake source: /nix/store/...-source)
-    # FIXME: DOES NOT WORK ANYMORE?!
-    linkToDots = pathRaw: let
-      pathStr = toString pathRaw;
-      flakeSourceDir = toString inputs.self; # same as `toString ./.` # Could be computed elsewhere
-      actualDotsDir = "${config.home.homeDirectory}/.dot"; # Could be set in a custom module?
-
-      pathRelativeToDots = lib.removePrefix (flakeSourceDir + "/") pathStr;
-      pathAbsoluteToDots = "${actualDotsDir}/${pathRelativeToDots}";
-    in config.lib.file.mkOutOfStoreSymlink pathAbsoluteToDots;
-  in linkToDots ../../git/config;
 
   # Do not build home-manager's manual, it brings a number of useless dependencies and I don't need
   # them often anyway.
---
+++ nix/homes/modules/dyndots.nix
@@ -0,0 +1,86 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.dyndots;
+in {
+  options = with lib; {
+    dyndots.mode = mkOption {
+      type = types.enum ["editable" "not-editable"];
+      default = "not-editable";
+    };
+    dyndots.dotfilesRepoPath = mkOption {
+      type = types.nullOr types.path; # something that coerces to an absolute path
+      description = "Path to the dotfiles repo, will be used for the out-of-store link";
+      example = ''"/home/user/path/to/dotfiles"'';
+      default = null;
+    };
+
+    # TODO(?): find a better option name?
+    # ideas:
+    # - selfInput
+    # - selfFlake
+    # - selfStorePath
+    # - pathToStrip
+    # - pureFlakePathToStrip
+    # - pureSelfSourcePath
+    # - pureFlakeSourcePath
+    # - pureSourcePath
+    # - ?
+    dyndots.flakeStorePath = mkOption {
+      # type = types.pathInStore;  # only available in unstable nixpkgs (not in 23.05)
+      # here is a copy/paste from more recent nixpkgs:
+      type = let
+        pathInStore = mkOptionType {
+          name = "pathInStore";
+          description = "path in the Nix store";
+          descriptionClass = "noun";
+          check = x: let
+            isStringLike = (x:
+              builtins.isString x
+              || builtins.isPath x
+              || x ? outPath
+              || x ? __toString
+            );
+          in isStringLike x && builtins.match "${builtins.storeDir}/[^.].*" (toString x) != null;
+          merge = mergeEqualOption;
+        };
+      in types.nullOr pathInStore;
+
+      description = ''
+        Since flake eval is pure, the source is moved to the store and all resolved ./relative paths
+        start in the store. This `flakeStorePath` will be used to strip the store location and get
+        back a real relative path from the flake (dotfiles directory).
+      '';
+    };
+
+    dyndots.mkLink = mkOption {
+      # type = types.functionTo types.package;  # FIXME: can't find a type that works..
+      description = "Entrypoint helper function to be used to make dyndots links (may be editable)";
+    };
+  };
+
+  config = let
+    mkStoreLink = givenPath: givenPath; # nothing to do, will be converted by home-manager
+    mkEditableDirectLink = givenPath: let
+      pathStr = toString givenPath;
+      # Remove store prefix from given path (if any) so that:
+      # given givenPath = ./foo
+      # then  pathStr = "/nix/store/aaaaaaa-the-flake-source/path/to/foo"
+      # then  pathRelativeToDots = "path/to/foo"
+      pathRelativeToDots = lib.removePrefix ((toString cfg.flakeStorePath) + "/") pathStr;
+    in
+      # note: builtin `assert` does not have a msg...
+      # https://github.com/NixOS/nix/issues/3233
+      assert lib.asserts.assertMsg (cfg.dotfilesRepoPath != null && cfg.flakeStorePath != null) ''
+        Options 'dotfilesRepoPath' and 'flakeStorePath' must be set for editable dyndots links
+      '';
+      # NOTE: Using mkOutOfStoreSymlink is necessary in home-manager (not in NixOS), it might be
+      #   easier in the future: https://github.com/nix-community/home-manager/issues/3032
+      config.lib.file.mkOutOfStoreSymlink "${cfg.dotfilesRepoPath}/${pathRelativeToDots}";
+  in {
+    dyndots.mkLink =
+      if cfg.mode == "editable"
+      then mkEditableDirectLink
+      else mkStoreLink;
+  };
+}
