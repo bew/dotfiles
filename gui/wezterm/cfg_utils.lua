d1f6676f1fc760941060ea3040d44ae3f42a2dd5
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2023-09-09 16:35:58 +0200

    wezterm: Use mods.D for WIN/CMD/SUPER

--- gui/wezterm/cfg_utils.lua
+++ gui/wezterm/cfg_utils.lua
@@ -1,6 +1,14 @@
 -- Define mods helper, to have a short way to create mods values
 -- (e.g: mods._ for NONE, mods.CA for CTRL|ALT)
-local mods = setmetatable({ _SHORT_MOD_MAP = { _ = "NONE", C = "CTRL", S = "SHIFT", A = "ALT", M = "SUPER" } }, {
+local mods = setmetatable({
+  _SHORT_MOD_MAP = {
+    _ = "NONE",
+    C = "CTRL",
+    S = "SHIFT",
+    A = "ALT",
+    D = "SUPER", -- D for Desktop (Win/Cmd/Super)
+  }
+}, {
   -- Dynamically transform key access of 'CSA' to 'CTRL|SHIFT|ALT'
   __index = function(self, key)
     local resolved_mods = self._SHORT_MOD_MAP[key:sub(1, 1)]
