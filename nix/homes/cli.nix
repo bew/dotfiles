6bf06c72056d2955231fa462ccc48e338189f84c
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2023-12-18 02:42:06 +0100

    git,nix: Add git-trim, git-absorb & onefetch git tools
    --- Closure diff:
    git-absorb: ∅ → 0.6.9, +2758.1 KiB
    git-trim: ∅ → 0.4.2, +4127.9 KiB
    onefetch: ∅ → 2.13.2, +13998.5 KiB
    git-bew-env: ∅ → ε, +113.2 KiB

--- nix/homes/cli.nix
+++ nix/homes/cli.nix
@@ -58,8 +58,11 @@ in {
         stable.git
         # config tools
         bleedingedge.delta # for nice git diffs
+        stable.onefetch # repo global info
         # extra commands
         stable.git-lfs # store specific (large) files out-of-repo
+        stable.git-trim # auto delete merged branches
+        stable.git-absorb # automatic `git commit --fixup` on relevant commits
         # other tools
         stable.gh # github cli for view & operations
       ];
