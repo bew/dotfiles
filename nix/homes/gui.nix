e75b9440f44abc739d994ed74a1fd9063b310860
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-10-16 04:08:19 +0200

    nix,gui: Remove GUI programs, moved to my nixos config (leave configs..)
    --- Closure diff (-470M !!)
    CopyQ: 7.1.0 → ∅, -9349.3 KiB
    arandr: 0.1.11 → ∅, -397.7 KiB
    brotli: -106.4 KiB
    cairo: -184.0 KiB
    cmake: 3.27.7 → ∅, -52625.2 KiB
    curl: -869.6 KiB
    dbus: -681.1 KiB
    dhcpcd: 9.4.1 → ∅, -478.6 KiB
    dnsmasq: 2.89 → ∅, -588.3 KiB
    double-conversion: 3.3.0 → ∅, -181.3 KiB
    duktape: 2.7.0 → ∅, -1226.3 KiB
    dunst: 1.9.2 → ∅, -277.2 KiB
    espanso: 2.1.8 → ∅, -16167.4 KiB
    ethtool: 6.1 → ∅, -595.6 KiB
    expat: -107.9 KiB
    ffmpeg: 4.4.4 → ∅, -25402.5 KiB
    fontconfig: -225.1 KiB
    freetype: -876.8 KiB
    glew: 2.2.0 → ∅, -971.2 KiB
    glib: -3518.0 KiB
    glibc: -2297.0 KiB
    glibc-iconv: 2.38 → ∅
    glu: 9.0.3 → ∅, -1479.9 KiB
    gobject-introspection-wrapped: 1.78.1 → ∅, -38.6 KiB
    hm_espanso: ∅ → ε
    home-manager: -90.5 KiB
    iproute2: 6.5.0 → ∅, -3970.6 KiB
    iputils: 20221126 → ∅, -472.4 KiB
    jansson: 2.14 → ∅, -112.6 KiB
    libX11: -250.6 KiB
    libXext: -58.8 KiB
    libXmu: 1.1.4 → ∅, -153.7 KiB
    libXrender: -35.3 KiB
    libappindicator-gtk3: 12.10.1+20.10.20200706.1 → ∅, -89.7 KiB
    libb2: 0.98.1 → ∅, -130.5 KiB
    libdbusmenu-gtk3: 16.04.0 → ∅, -624.6 KiB
    libedit: 20221030-3.1 → ∅, -282.9 KiB
    libelf: 0.8.13 → ∅, -335.2 KiB
    libffi: -20.8 KiB
    libidn: 1.41 → ∅, -411.5 KiB
    libmbim: 1.28.4 → ∅, -1361.0 KiB
    libndp: 1.8 → ∅, -75.7 KiB
    libnotify: 0.8.3 → ∅, -99.2 KiB
    libpng-apng: -255.1 KiB
    libproxy: 0.4.18 → ∅, -401.7 KiB
    libqmi: 1.32.4 → ∅, -6432.0 KiB
    libqrtr-glib: 1.2.2 → ∅, -59.1 KiB
    libxcb: -2096.5 KiB
    libxkbfile: 1.1.2 → ∅, -170.6 KiB
    linux-headers: 6.5 → ∅, -6353.1 KiB
    md4c: 0.4.8 → ∅, -271.2 KiB
    mobile-broadband-provider-info: 20230416 → ∅, -511.5 KiB
    modemmanager: 1.20.6 → ∅, -12610.1 KiB
    net-tools: 2.10 → ∅, -509.9 KiB
    networkmanager: 1.44.2 → ∅, -18053.6 KiB
    newt: 0.52.23 → ∅, -469.1 KiB
    nftables: 1.0.9 → ∅, -1128.4 KiB
    openconnect: 9.12 → ∅, -629.7 KiB
    openresolv: 3.13.2 → ∅, -66.8 KiB
    perl5.38.2-File-BaseDir: 0.09 → ∅, -18.9 KiB
    perl5.38.2-File-DesktopEntry: 0.22 → ∅, -27.2 KiB
    perl5.38.2-File-MimeInfo: 0.33 → ∅, -78.6 KiB
    perl5.38.2-IPC-System-Simple: 1.30 → ∅, -36.5 KiB
    perl5.38.2-Net-DBus: 1.2.0 → ∅, -465.3 KiB
    perl5.38.2-X11-Protocol: 0.56 → ∅, -244.4 KiB
    perl5.38.2-XML-Parser: 2.46 → ∅, -428.7 KiB
    perl5.38.2-XML-Twig: 3.52 → ∅, -502.7 KiB
    pkg-config: 0.29.2 → ∅, -645.1 KiB
    pkg-config-wrapper: 0.29.2 → ∅, -11.0 KiB
    polkit: 123 → ∅, -535.5 KiB
    ppp: 2.5.0 → ∅, -992.0 KiB
    python3.11-dbus-python: 1.2.18 → ∅, -710.4 KiB
    python3.11-pycairo: 1.24.0 → ∅, -488.9 KiB
    python3.11-pygobject: 3.46.0 → ∅, -1192.5 KiB
    python3.11-pyxdg: 0.28 → ∅, -643.7 KiB
    qtbase: 6.6.1 → ∅, -71161.0 KiB
    qtdeclarative: 6.6.1 → ∅, -106097.7 KiB
    qtlanguageserver: 6.6.1 → ∅, -2213.6 KiB
    qtquickcontrols: 5.15.12 → ∅, -5371.2 KiB
    qtsvg: 6.6.1 → ∅, -812.6 KiB
    qttools: 5.15.12, 6.6.1 → ∅, -34897.8 KiB
    qttranslations: 6.6.1 → ∅, -13359.8 KiB
    qtwayland: 5.15.12, 6.6.1 → ∅, -16377.7 KiB
    qtx11extras: 5.15.12 → ∅, -32.4 KiB
    redshift: 1.12 → ∅, -922.8 KiB
    rhash: 1.4.4 → ∅, -354.3 KiB
    screenkey: 1.5 → ∅, -748.8 KiB
    setxkbmap: 1.3.4 → ∅, -38.4 KiB
    simplescreenrecorder: 0.4.3 → ∅, -4173.2 KiB
    slang: 2.3.3 → ∅, -2716.1 KiB
    slop: 7.6 → ∅, -471.1 KiB
    stoken: 0.93 → ∅, -219.8 KiB
    strip.sh: ε → ∅
    unixODBC: 2.3.12 → ∅, -1084.8 KiB
    vpnc-scripts-unstable: 2023-01-03 → ∅, -40.5 KiB
    wxwidgets: 3.2.4 → ∅, -32368.4 KiB
    xcb-util-cursor: 0.1.4 → ∅, -29.1 KiB
    xclip: 0.13 → ∅, -41.4 KiB
    xdg-utils-unstable: 2022-11-06 → ∅, -293.8 KiB
    xdotool: 3.20211022.1 → ∅, -195.8 KiB
    xorgproto: 2023.2 → ∅, -1639.5 KiB
    xrandr: 1.5.2 → ∅, -77.8 KiB
    xset: 1.2.5 → ∅, -45.2 KiB
    zlib: -112.7 KiB

--- nix/homes/gui.nix
+++ nix/homes/gui.nix
@@ -6,34 +6,7 @@ in {
   imports = [
     ./gui-fix-xdg-data-dirs.nix
     ./gui-force-system-locales.nix
-    ./gui-espanso-text-expander.nix
   ];
 
-  # Text expansion!
-  my.services.espanso = {
-    enable = true;
-    package = stable.espanso;
-    configDir = config.dyndots.mkLink ../../gui/espanso;
-  };
-
-  home.packages = [
-    # desktop/wm related (TODO? nixify config)
-    stable.dunst
-    # FIXME: herbstluftwm is missing here (can't get it to compile last version)
-
-    # X tools
-    stable.copyq # powerful clipboard manager
-    stable.xclip
-    stable.xdotool
-
-    stable.arandr
-
-    (stable.redshift.override { withGeolocation = false; })
-
-    # screen/video capture
-    stable.simplescreenrecorder # simple (to use) yet powerful Screen Recorder
-    stable.screenkey
-    stable.slop
-    # stable.peek ? (for gifs iirc)
-  ];
+  xdg.configFile."espanso".source = config.dyndots.mkLink ../../gui/espanso;
 }
