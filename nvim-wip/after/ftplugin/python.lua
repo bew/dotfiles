e77dbde8c37062ab35758cd402c280c2114dd1e6
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-10-21 22:01:17 +0200

    nvim: Convert python ftplugin to Lua

--- nvim-wip/after/ftplugin/python.lua
+++ nvim-wip/after/ftplugin/python.lua
@@ -54,3 +54,39 @@ if vim.fn.executable("pylsp") == 1 then
 else
   vim.notify("LSP not found")
 end
+
+-------------------------------------------------------------------------------
+
+-- I: <Alt-:> to insert a colon after cursor.
+toplevel_buf_map{mode="i", key=[[<M-:>]], action=[[: <C-g>U<Left><C-g>U<Left>]]}
+
+-- I: <Alt-f> to toggle between `f"..."` and `"..."`
+-- NOTE: it is pretty dumb, searching the first '"' on left of cursor.
+-- TODO: check with tree-sitter if in string + add support for multiline string
+local function fstring_toggle()
+  local saved_cursor = vim.api.nvim_win_get_cursor(0) -- (1, 0)-indexed
+  local cur_row0, cur_col0 = saved_cursor[1] -1, saved_cursor[2]
+  local line_before = vim.api.nvim_get_current_line():sub(1, cur_col0 +1)
+  local quote_rel_idx1 = line_before:reverse():find('"')
+  if not quote_rel_idx1 then
+    vim.notify([[Cannot toggle f-string, not in string? 👀]])
+    return
+  end
+  local quote_idx1 = #line_before - quote_rel_idx1 + 1
+  local f_idx1 = quote_idx1 -1
+  local is_fstring = line_before:sub(f_idx1, f_idx1) == "f"
+
+  if is_fstring then
+    -- Delete `f`
+    local row0 = cur_row0
+    local col0 = f_idx1 -1
+    vim.api.nvim_buf_set_text(0, row0, col0, row0, col0 +1, {}) -- 0-indexed
+    vim.api.nvim_win_set_cursor(0, cur_row0 +1, ) -- (1, 0)-indexed
+  else
+    -- Insert `f` at quote position
+    local row0 = cur_row0
+    local col0 = quote_idx1 -1
+    vim.api.nvim_buf_set_text(0, row0, col0, row0, col0, {"f"})
+  end
+end
+toplevel_buf_map{mode="i", key=[[<M-f>]], action=fstring_toggle}
