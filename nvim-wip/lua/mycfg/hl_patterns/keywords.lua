fb400821fc5dc9f1b047a978647730ce71671cee
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2024-12-15 06:40:45 +0100

    nvim(hipat): Simplify big_word patterns def, add few more

--- nvim-wip/lua/mycfg/hl_patterns/keywords.lua
+++ nvim-wip/lua/mycfg/hl_patterns/keywords.lua
@@ -1,38 +1,40 @@
 
-local U = require"mycfg.hl_patterns.utils"
+local U = require"mylib.utils"
+local _U = require"mycfg.hl_patterns.utils"
 
 local patterns = {}
+
+--- Give keyword variants for FOO:
+--- - `FOO`
+--- - `FOO:`
+local function _big_word_variants(big_word)
+  return {
+    _U.keywordize(big_word),
+    _U.keywordize(big_word .. ":"),
+  }
+end
 
 ------- Group: General keywords
 
 patterns.big_todo = {
-  pattern = {
-    U.keywordize"TODO",
-    U.keywordize"TODO:",
-  },
-  group = U.define_hl("big_todo", {
+  pattern = _big_word_variants"TODO",
+  group = _U.define_hl("big_todo", {
     ctermfg = 11,
     bold = true,
   }),
 }
 
 patterns.big_note = {
-  pattern = {
-    U.keywordize"NOTE",
-    U.keywordize"NOTE:",
-  },
-  group = U.define_hl("big_note", {
+  pattern = _big_word_variants"NOTE",
+  group = _U.define_hl("big_note", {
     ctermfg = 32,
     underline = true,
   }),
 }
 
 patterns.big_fixme = {
-  pattern = {
-    U.keywordize"FIXME",
-    U.keywordize"FIXME:",
-  },
-  group = U.define_hl("big_fixme", {
+  pattern = _big_word_variants"FIXME",
+  group = _U.define_hl("big_fixme", {
     ctermfg = 160,
     underline = true,
     bold = true,
@@ -40,35 +42,40 @@ patterns.big_fixme = {
 }
 
 patterns.big_idea = {
-  pattern = {
-    U.keywordize"IDEA",
-    U.keywordize"IDEA:",
-  },
-  group = U.define_hl("big_idea", {
+  pattern = _big_word_variants"IDEA",
+  group = _U.define_hl("big_idea", {
     ctermfg = 128,
     bold = true,
   }),
 }
 
 patterns.big_maybe = {
-  pattern = {
-    U.keywordize"MAYBE",
-    U.keywordize"MAYBE:",
-  },
-  group = U.define_hl("big_maybe", {
+  pattern = _big_word_variants"MAYBE",
+  group = _U.define_hl("big_maybe", {
     ctermfg = 128,
     bold = true,
   }),
 }
 
-patterns.big_fail = {
-  pattern = {
-    U.keywordize"FAIL",
-    U.keywordize"FAIL:",
+patterns.big_fail_bad = {
+  pattern = U.concat_lists {
+    _big_word_variants"FAIL",
+    _big_word_variants"BAD",
   },
-  group = U.define_hl("big_fixme", {
+  group = _U.define_hl("big_fail_bad", {
     ctermfg = 124,
     bold = true,
+  }),
+}
+
+patterns.big_warning = {
+  pattern = U.concat_lists {
+    _big_word_variants"WARN",
+    _big_word_variants"WARNING",
+  },
+  group = _U.define_hl("big_warning", {
+    ctermfg = 11,
+    italic = true,
   }),
 }
 
@@ -77,51 +84,56 @@ patterns.big_fail = {
 patterns.sym_question = {
   -- note: Match `BLA(?)`
   pattern = "%(%?%)",
-  group = U.define_hl("sym_question", {
+  group = _U.define_hl("sym_question", {
     ctermfg = 128,
+    bold = true,
+  }),
+}
+
+patterns.sym_excl = {
+  -- note: Match `BLA(!!)`
+  pattern = "%(!!%)",
+  group = _U.define_hl("sym_excl", {
+    ctermfg = 124,
     bold = true,
   }),
 }
 
 ------- Group: Misc keywords
 
-local default_hl = U.define_hl("default", {
-  ctermfg = 255,
-  bold = true,
-})
 
-patterns.misc_keywords = {
+patterns.misc_big_words = {
   pattern = {
-    U.keywordize"DOC:",
-    U.keywordize"REF:",
-    U.keywordize"RELATED:",
-    U.keywordize"SEE:",
-    U.keywordize"TOTRY:",
+    _U.keywordize"DOC:",
+    _U.keywordize"REF:",
+    _U.keywordize"RELATED:",
+    _U.keywordize"SEE:",
+    _U.keywordize"TOTRY:",
   },
-  group = default_hl,
+  group = _U.define_hl("misc_big_words", {
+    ctermfg = 255,
+    bold = true,
+  }),
 }
 
 patterns.faq = {
-  pattern = {
-    U.keywordize"FAQ",
-    U.keywordize"FAQ:",
-  },
-  group = U.define_hl("faq", {
+  pattern = _big_word_variants"FAQ",
+  group = _U.define_hl("faq", {
     ctermfg = 129,
     bold = true,
   }),
 }
 
 patterns.QnA_Q = {
-  pattern = U.keywordize"Q:",
-  group = U.define_hl("QnA_Q", {
+  pattern = _U.keywordize"Q:",
+  group = _U.define_hl("QnA_Q", {
     ctermfg = 202,
     bold = true,
   })
 }
 patterns.QnA_A = {
-  pattern = U.keywordize"A:",
-  group = U.define_hl("QnA_A", {
+  pattern = _U.keywordize"A:",
+  group = _U.define_hl("QnA_A", {
     ctermfg = 34,
     bold = true,
   })
