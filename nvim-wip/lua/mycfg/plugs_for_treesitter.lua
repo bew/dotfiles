9c53801c858591e486187e8f2c4ebdddfb290b40
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-01-05 06:21:51 +0100

    nvim: Add treesj plugin for easy split/join of TS blocks ✨

--- nvim-wip/lua/mycfg/plugs_for_treesitter.lua
+++ nvim-wip/lua/mycfg/plugs_for_treesitter.lua
@@ -23,3 +23,33 @@ Plug.ts {
     }
   end,
 }
+
+Plug {
+  source = gh"Wansmer/treesj",
+  desc = "Neovim plugin for splitting/joining blocks of code",
+  tags = {t.ts, t.editing},
+  defer_load = { on_event = "VeryLazy" },
+  on_load = function()
+    local tsj = require"treesj"
+    tsj.setup {
+      use_default_keymaps = false,
+    }
+
+    my_actions.treesj_toggle = mk_action_v2 {
+      default_desc = "[Treesj] Toggle TS block split/join",
+      n = tsj.toggle,
+    }
+    my_actions.treesj_split = mk_action_v2 {
+      default_desc = "[Treesj] Split TS block",
+      n = tsj.split,
+    }
+    my_actions.treesj_join = mk_action_v2 {
+      default_desc = "[Treesj] Join TS block",
+      n = tsj.join,
+    }
+    local_leader_map_define_group{mode={"n"}, prefix_key="j", name="+split/join"}
+    local_leader_map{mode={"n"}, key="j<Space>", action=my_actions.treesj_toggle}
+    local_leader_map{mode={"n"}, key="js", action=my_actions.treesj_split}
+    local_leader_map{mode={"n"}, key="jj", action=my_actions.treesj_join}
+  end
+}
