f8a13275712223711d4eae929388db7daaded172
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-01-05 07:27:00 +0100

    nvim: oil: Add winbar with current dir for that Oil buffer

--- nvim-wip/lua/mycfg/plugs_for_ui.lua
+++ nvim-wip/lua/mycfg/plugs_for_ui.lua
@@ -124,8 +124,22 @@ Plug {
   tags = {"filesystem"},
   -- NOTE: can't use defer_load, or it wouldn't hijack netrw on `nvim <dir>`
   on_load = function()
+    -- NOTE: taken from: https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md
+    function _G._oil_get_winbar()
+      local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+      local dir = require"oil".get_current_dir(bufnr)
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
     require"oil".setup {
       default_file_explorer = true,
+      win_options = {
+        winbar = "%!v:lua._oil_get_winbar()",
+      },
 
       keymaps = {
         -- Disable default split/tab actions that use Ctrl 😬
