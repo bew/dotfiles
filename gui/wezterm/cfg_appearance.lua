070bbc65cc96d61be0f1309fe43b3a2ca1d5d546
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-02-05 01:03:57 +0100

    wezterm: Tweak win appearance when using multiple panes

--- gui/wezterm/cfg_appearance.lua
+++ gui/wezterm/cfg_appearance.lua
@@ -5,14 +5,13 @@ cfg.hide_tab_bar_if_only_one_tab = true
 -- Pad window to avoid the content to be too close to the border,
 -- so it's easier to see and select.
 cfg.window_padding = {
-  left = 3, right = 3,
-  top = 3, bottom = 3,
+  left = 5, right = 5,
+  top = 5, bottom = 5,
 }
 
 cfg.inactive_pane_hsb = {
   -- NOTE: these values are multipliers, applied on normal pane values
-  saturation = 0.9,
-  brightness = 0.6,
+  brightness = 0.7,
 }
 
 cfg.colors = require("cfg_bew_colors")
