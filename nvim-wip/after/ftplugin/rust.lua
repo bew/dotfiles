fe3fa341ff9fe0f169278bf31ea96bf439a68e14
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-02-05 06:59:38 +0100

    nvim(rust): Default to 4 spaces indent

--- nvim-wip/after/ftplugin/rust.lua
+++ nvim-wip/after/ftplugin/rust.lua
@@ -1,4 +1,6 @@
 -- Force disable auto-comment-leader on 'o' or 'O'.
 vim.opt_local.formatoptions:remove { "o" }
 
+vim.opt_local.shiftwidth = 4
+
 -- NOTE: LSP Setup is done by https://github.com/mrcjkb/rustaceanvim
