2dc50e9cf3dfdb5e60351e5d4a82a504b301a993
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-03-20 11:04:10 +0000

    nvim(snip:py): Add MANY snippets (ported from my old Ultisnips config)

--- nvim-wip/lua/mycfg/snippets_by_ft/python.lua
+++ nvim-wip/lua/mycfg/snippets_by_ft/python.lua
@@ -3,6 +3,9 @@ local ls = require"luasnip"
 local SU = require"mycfg.snippets_by_ft._utils" -- Snip Utils
 local conds = require"mycfg.snippets_by_ft._conditions"
 
+local ls_extras = require"luasnip.extras"
+ls_extras.repeat_node = ls_extras.rep
+
 local SNIPS = {}
 local snip = SU.get_snip_fn(SNIPS)
 
@@ -10,6 +13,8 @@ local i = ls.insert_node ---@diagnostic disable-line: unused-local
 local t = ls.text_node ---@diagnostic disable-line: unused-local
 
 -- Start of snippets definitions
+
+-- TODO: Split snips in multiple files for **large** topics 🤔
 
 snip("cl", {desc = "class def", when = conds.very_start_of_line}, SU.myfmt {
   [[
@@ -26,7 +31,7 @@ snip("cl", {desc = "class def", when = conds.very_start_of_line}, SU.myfmt {
   }
 })
 
-snip("data", {desc = "dataclass def", when = conds.very_start_of_line}, SU.myfmt {
+snip("da", {desc = "dataclass def", when = conds.very_start_of_line}, SU.myfmt {
   [[
     @dataclass<maybe_decor_params>
     class <name><maybe_parents>:
@@ -46,14 +51,29 @@ snip("data", {desc = "dataclass def", when = conds.very_start_of_line}, SU.myfmt
   }
 })
 
-snip("fromd", {desc = "import for @dataclass", when = conds.very_start_of_line}, {
+snip("fda", {desc = "import for @dataclass", when = conds.very_start_of_line}, {
   t[[from dataclasses import dataclass]]
 })
 
 -------------------
 
---- Returns method def snippet for given *opts*
----@param opts any
+-- MAYBE: contribute this snippet generator to LuaSnip's Wiki? 🤔
+
+---@class mysnips.py.DefOpts
+---@field decor_name string? Decorator name (if needed)
+---@field name string? The def name (not editable)
+---@field default_name string? The default def name (editable)
+---@field first_arg_name string|false? Name of the first arg (e.g. `self`), or `false`
+---@field maybe_arg boolean? Whether def may have more args (alt choice to remove it)
+---@field maybe_return_type boolean? Whether def may have return type (alt choice to remove it)
+---@field needs_return_type boolean? Whether def must have return type
+---@field maybe_no_cover boolean? Whether def may have `# pragma: no cover` (for abstract defs)
+---@field simple_body_node (fun(idx_fn: (fun(): integer)): SnipNodeT)?
+---    Function that should return a snippet node for the body, takes an index factory function.
+---    If not given, it'll default to `pass`.
+
+--- Returns method def snippet for given options
+---@param opts mysnips.py.DefOpts
 ---@return SnipNodeT
 local function make_def_snip(opts)
   opts = opts or {}
@@ -100,7 +120,7 @@ local function make_def_snip(opts)
           ls.snippet_node(nil, {t" -> ", i(1, "Any")}),
           t"",
         })
-        or opts.return_type and ls.snippet_node(next_insert_idx(), {t" -> ", i(1, "Any")})
+        or opts.needs_return_type and ls.snippet_node(next_insert_idx(), {t" -> ", i(1, "Any")})
         or t"" -- nothing
       ),
       maybe_no_cover = (
@@ -119,8 +139,7 @@ local function make_def_snip(opts)
 end
 
 snip(
-  "def",
-  {desc = "function def", when = conds.very_start_of_line},
+  "def", {desc = "function def", when = conds.very_start_of_line},
   make_def_snip {
     default_name = "function_name",
     first_arg_name = false, -- no first arg
@@ -135,8 +154,7 @@ snip(
 --     pass
 -- ```
 snip(
-  "def",
-  {desc = "method def", when = conds.after_indent},
+  "def", {desc = "method def", when = conds.after_indent},
   make_def_snip {
     default_name = "method_name",
     first_arg_name = "self",
@@ -284,57 +302,94 @@ snip(
 
 -------------------
 
--- TODO: Split snips in multiple files for **large** topics
-
--- TODO: #!  =>  python shebang
--- TODO: foo=  =>  foo=foo
--- TODO: deft  =>  test function
+-- TODO: deft  =>  test functionvim.split("foo", "%.")[1]
 -- TODO: deftx  =>  pytest fixture function
--- TODO: ifmain  =>  if name is main ...
--- TODO: mainsimple  =>  basic main fn
--- TODO: for  =>  for loop
--- TODO: try  =>  try except block
--- TODO: pp  =>  pretty print (inline import)
--- TODO: f  =>  f"blabla"
--- TODO: s  =>  self.X = X
--- TODO: s_  =>  self._X = X
--- TODO: opt  =>  Foo | None
--- TODO: optn  =>  Foo | None = None
--- TODO: ld  =>  lambda
--- TODO: bp  =>  breakpoint()
--- TODO: bpt  =>  pytest breakpoint
--- TODO: rty  =>  reveal_type(…)
 -- TODO: pytr  =>  pytest check raise
 -- TODO: pytp  =>  pytest parametrize
 -- TODO: pyts  =>  pytest mark skip
 
+snip("([%w_]+)=", {desc = "arg=arg auto-fill", trigEngine = "pattern", wordTrig = false}, SU.myfmt {
+  [[<param>=<param>]],
+  {
+    param = ls.function_node(function(_args, snip)
+      local snip = snip ---@type SnipT
+      return snip.env.LS_CAPTURE_1
+    end),
+  },
+})
 
 -- Docstrings-related snips
 snip("dd", {desc = [["""Doc…"""]]}, SU.myfmt {
   [["""<doc>"""]],
   { doc = i(1, "TODO: doc!") }
 })
-snip(":p", {desc = ":param <foo>: …"}, SU.myfmt {
+snip(":p", {desc = ":param <foo>: …", when = conds.start_of_line}, SU.myfmt {
   [[:param <param>: <desc>]],
   {
     param = i(1, "my_param"),
     desc = i(2, "TODO: describe!"),
   }
 })
-snip(":x", {desc = ":raises <Error>: …"}, SU.myfmt {
+snip(":x", {desc = ":raises <Error>: …", when = conds.start_of_line}, SU.myfmt {
   [[:raises <errors>: <desc>]],
   {
     errors = i(1, "my_param"),
     desc = i(2, "TODO: describe!"),
   }
 })
-snip(":r", {desc = ":return: …"}, t":return: ")
+snip(":r", {desc = ":return: …", when = conds.start_of_line}, t":return: ")
 
 -------------------
 
-snip("p", {desc = "print(...)"}, SU.myfmt {
+snip("f", {desc = "f-string"}, SU.myfmt {
+  [[f"<msg>"]],
+  { msg = i(1) },
+})
+
+snip("p", {desc = "print(…)"}, SU.myfmt {
   [[print(<stuff>)]],
   { stuff = i(1) }
+})
+
+snip("pf", {desc = "print(f-string…)"}, SU.myfmt {
+  [[print(f"<msg>"<rest>)]],
+  {
+    msg = i(1),
+    rest = i(2),
+  },
+})
+
+snip("pp", {desc = "debug pretty print (…)", when = conds.start_of_line}, SU.myfmt {
+  [[__import__("pprint").pprint(<expr>)  # FIXME: REMOVE DEBUG! # noqa]],
+  { expr = SU.insert_node_default_selection(1) }
+})
+
+-- GRR: it's reeaalllyyyy lloooonnnggg 😬
+snip("ppj", {desc = "debug pretty print data as json", when = conds.start_of_line}, SU.myfmt {
+  [[print(__import__("json").dumps(<expr>, indent=4, sort_keys=True))  # FIXME: REMOVE DEBUG! # noqa]],
+  { expr = SU.insert_node_default_selection(1, "data") }
+})
+
+snip("s", {desc = "self.X = X", when = conds.start_of_line}, SU.myfmt {
+  [[self.<name> = <name_again>]],
+  {
+    name = i(1, "name"),
+    name_again = ls.choice_node(2, {
+      ls_extras.repeat_node(1), -- same name by default
+      i(nil),
+    }),
+  },
+})
+
+snip("s_", {desc = "self._X = X", when = conds.start_of_line}, SU.myfmt {
+  [[self._<name> = <name_again>]],
+  {
+    name = i(1, "name"),
+    name_again = ls.choice_node(2, {
+      ls_extras.repeat_node(1), -- same name by default
+      i(nil),
+    }),
+  },
 })
 
 -- NOTE: Adds the space after `return` only if needed,
@@ -342,7 +397,7 @@ snip("p", {desc = "print(...)"}, SU.myfmt {
 snip(
   "r",
   {
-    desc = "return ...",
+    desc = "return …",
     -- Tweak what will be removed exactly before snip expansion
     -- Remove extra spaces after trigger, to always leave cursor 'after space' after snip expansion.
     resolveExpandParams = SU.mk_expand_params_resolver { delete_after_trig = "^%s+" },
@@ -351,6 +406,209 @@ snip(
     t"return "
   }
 )
+
+snip("ld", {desc = "lambda"}, t"lambda")
+
+snip("try", {desc = "try … except …", when = conds.start_of_line}, SU.myfmt {
+  [[
+    try:
+        <body>
+    except <ex_type><maybe_ex_name>:
+        <handler>
+  ]],
+  {
+    body = SU.insert_node_default_selection(1, "# do something useful.."),
+    ex_type = i(2, "SomeException"),
+    maybe_ex_name = ls.choice_node(3, {
+      ls.snippet_node(nil, {t" as ", i(1, "err")}),
+      t"",
+    }),
+    handler = i(4, "pass"),
+  }
+})
+
+snip("ex", {desc = "except …", when = conds.start_of_line}, SU.myfmt {
+  [[
+    except <ex_type><maybe_ex_name>:
+        <handler>
+  ]],
+  {
+    ex_type = i(1, "SomeException"),
+    maybe_ex_name = ls.choice_node(2, {
+      ls.snippet_node(nil, {t" as ", i(1, "err")}),
+      t"",
+    }),
+    handler = i(3, "pass"),
+  }
+})
+
+snip("forkv", {desc = "for loop over keys", when = conds.start_of_line}, SU.myfmt {
+  [[
+    for <key>, <value> in <dict>.items():
+        <body>
+  ]],
+  {
+    dict = i(1, "somedict"),
+    key = i(2, "_key"),
+    value = i(3, "_value"),
+    body = i(4, "pass"),
+  },
+})
+
+snip("forn", {desc = "for n in range", when = conds.start_of_line}, SU.myfmt {
+  [[
+    for <item> in range(<range_args>):
+        <body>
+  ]],
+  {
+    item = i(1, "item"),
+    range_args = ls.choice_node(2, {
+      i(nil, "num_iterations"),
+      ls.snippet_node(nil, SU.myfmt {
+        "<start>, <end_>",
+        {
+          start = ls.restore_node(1, "start"),
+          end_ = ls.restore_node(2, "end_"),
+        }
+      }),
+      ls.snippet_node(nil, SU.myfmt {
+        "<start>, <end_>, <step>",
+        {
+          start = ls.restore_node(1, "start"),
+          end_ = ls.restore_node(2, "end_"),
+          step = i(3, "-1"),
+        },
+      }),
+    }, {
+      -- Seemlessly keep cursor pos across choice branches
+      restore_cursor = true
+    }),
+    body = i(3, "pass"),
+  },
+}, {
+  stored = {
+    -- Keys for ls.restore_node
+    -- (used to share nodes between choice node branches)
+    start = i(nil, "1"),
+    end_ = i(nil, "10"),
+  },
+})
+
+snip("for", {desc = "for item in iterable", when = conds.start_of_line}, SU.myfmt {
+  [[
+    for <item> in <iterable>:
+        <body>
+  ]],
+  {
+    item = i(1, "item"),
+    iterable = i(2, "someiterable"),
+    body = i(3, "pass"),
+  },
+})
+
+snip("o", {desc = "Foo | None"}, SU.myfmt {
+  [[<type> | None]],
+  { type = SU.insert_node_default_selection(1, "SomeType") }
+})
+
+snip("on", {desc = "Foo | None = None"}, SU.myfmt {
+  [[<type> | None = None]],
+  { type = SU.insert_node_default_selection(1, "SomeType") }
+})
+
+snip("rt", {desc = "reveal type", when = conds.start_of_line}, SU.myfmt {
+  [[reveal_type(<expr>)  # noqa: F821 # TODO: remove probe]],
+  { expr = SU.insert_node_default_selection(1) }
+})
+snip("rt", {desc = "reveal type (in expr)"}, SU.myfmt {
+  [[reveal_type(<expr>)]],
+  { expr = SU.insert_node_default_selection(1) }
+})
+
+snip("bp", {desc = "Add breakpoint here"}, SU.myfmt {
+  [[breakpoint()<maybe_reminder>]],
+  {
+    maybe_reminder = ls.choice_node(1, {
+      t"  # FIXME: REMOVE DEBUG PROBE!",
+      t"",
+    }),
+  },
+})
+snip("bpt", {desc = "Add pytest breakpoint"}, SU.myfmt {
+  [[__import__("pytest").set_trace()<maybe_reminder>]],
+  {
+    maybe_reminder = ls.choice_node(1, {
+      t"  # FIXME: REMOVE DEBUG PROBE!",
+      t"",
+    }),
+  },
+})
+
+snip("maincli", {desc = "minimal setup for main(args) & cli parsing", when = conds.very_start_of_line}, SU.myfmt {
+  -- NOTE: the `->>` is just a way to escape `>` to avoid having it
+  -- NOTE: `move_to_top` is not on the first instance, because having insert mode just after an
+  --   identifier triggers the completion popup, which is absolutely not useful ><
+  [[
+    import argparse<move_to_top_again>
+    import sys<move_to_top_again>
+
+
+    class ScriptError(Exception):<move_to_top>
+        pass
+
+
+    def parse_args(args) ->> argparse.Namespace:
+        parser = argparse.ArgumentParser()
+        return parser.parse_args(args)
+
+
+    def main(args):
+        opts = parse_args(args)
+        <body>
+
+
+    if __name__ == "__main__":
+        try:
+            main(sys.argv[1:])
+        except ScriptError as err:
+            print(f"ERROR: {err}", file=sys.stderr)
+            sys.exit(1)<after>
+  ]],
+  {
+    move_to_top = ls.choice_node(1, {
+      t"  # TODO: move to top!",
+      t"",
+    }),
+    move_to_top_again = ls_extras.repeat_node(1),
+    body = SU.insert_node_default_selection(2, "# do something useful!"),
+    after = i(3),
+  },
+})
+
+snip("mainsimple", {desc = "simple main() script", when = conds.very_start_of_line}, SU.myfmt {
+  [[
+    def main():
+        <body>
+
+
+    if __name__ == "__main__":
+        main()<after>
+  ]],
+  {
+    body = SU.insert_node_default_selection(1, "pass"),
+    after = i(2),
+  }
+})
+
+snip("ifmain", {desc = "if module is main", when = conds.very_start_of_line}, SU.myfmt {
+  [[
+    if __name__ == "__main__":
+        <body>
+  ]],
+  {
+    body = SU.insert_node_default_selection(1, "pass"),
+  }
+})
 
 -- End of snippets definitions
 
