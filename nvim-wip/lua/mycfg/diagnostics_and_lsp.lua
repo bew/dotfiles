2cbd82002d6de0956d5a70dd4bd8681540581f2a
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-03-20 22:21:20 +0000

    nvim(diags): Upgrade goto next/prev actions to go by layer of severities

--- nvim-wip/lua/mycfg/diagnostics_and_lsp.lua
+++ nvim-wip/lua/mycfg/diagnostics_and_lsp.lua
@@ -7,24 +7,85 @@ vim.diagnostic.config {
 -- TODO: hide diag virtual _TEXT_ most of the time, but show colored dots only (or types of diags),
 --   and only show the current line diag text (in virtual text / in cmdline / in statusline 🤔)
 
+---@class mycfg.diag.GotoDiagBySevArgs
+---@field goto_diag_fn fun(opts?: vim.diagnostic.GotoOpts)
+---@field get_diag_pos_fn fun(opts?: vim.diagnostic.GotoOpts)
+---@field goto_opts vim.diagnostic.GotoOpts
+
+--- Jump to next/prev diagnostic by layer of severity.
+--- Search for ERROR severity, if none search for WARN severity, if none search for HINT severity.
+---@param args mycfg.diag.GotoDiagBySevArgs
+local goto_diag_by_severity_layers = function(args)
+  local goto_opts = args.goto_opts
+  for _, severity in ipairs({"ERROR", "WARN", "HINT"}) do
+    goto_opts.severity = vim.diagnostic.severity[severity]
+    if args.get_diag_pos_fn(goto_opts) then
+      -- There is a diagnostic for this severity, jump to it
+      args.goto_diag_fn(goto_opts)
+      return
+    end
+    -- otherwise, check the next severity..
+    print("no more diag with severity "..severity..", checking next severity..", vim.log.levels.DEBUG)
+  end
+  vim.notify("Zero diagnostics left 👍", vim.log.levels.INFO)
+end
+
+my_actions.goto_next_diag = mk_action_v2 {
+  default_desc = "Go to next diagnostic (by severity)",
+  n = function()
+    goto_diag_by_severity_layers {
+      get_diag_pos_fn = vim.diagnostic.get_next_os,
+      goto_diag_fn = vim.diagnostic.goto_next,
+      goto_opts = {}, -- defaults
+    }
+  end,
+}
+
+my_actions.goto_prev_diag = mk_action_v2 {
+  default_desc = "Go to prev diagnostic (by severity)",
+  n = function()
+    goto_diag_by_severity_layers {
+      get_diag_pos_fn = vim.diagnostic.get_prev_os,
+      goto_diag_fn = vim.diagnostic.goto_prev,
+      goto_opts = {}, -- defaults
+    }
+  end,
+}
+
+my_actions.goto_next_diag_diff_line = mk_action_v2 {
+  default_desc = "Go to next diagnostic (by severity, != line)",
+  n = function()
+    goto_diag_by_severity_layers {
+      get_diag_pos_fn = vim.diagnostic.get_next_os,
+      goto_diag_fn = vim.diagnostic.goto_next,
+      goto_opts = {
+        -- Start searching from next line
+        cursor_position = { --[[ row1 ]] vim.fn.line('.') + 1, --[[ col0 ]] 0 },
+      },
+    }
+  end,
+}
+
+my_actions.goto_prev_diag_diff_line = mk_action_v2 {
+  default_desc = "Go to prev diagnostic (by severity, != line)",
+  n = function()
+    goto_diag_by_severity_layers {
+      get_diag_pos_fn = vim.diagnostic.get_prev_pos,
+      goto_diag_fn = vim.diagnostic.goto_prev,
+      goto_opts = {
+        -- Start searching from prev line
+        cursor_position = { --[[ row1 ]] vim.fn.line('.') - 1, --[[ col0 ]] 0 },
+      },
+    }
+  end,
+}
+
 local_leader_map_define_group{mode={"n"}, prefix_key="d", name="+diagnostics"}
 local_leader_map{mode="n", key="dd", desc="Show diagnostics in popup", action=vim.diagnostic.open_float}
-local_leader_map{mode="n", key="dn", desc="Go to next diagnostic", action=vim.diagnostic.goto_next}
-local_leader_map{mode="n", key="dp", desc="Go to prev diagnostic", action=vim.diagnostic.goto_prev}
-local_leader_map{mode="n", key="dj", desc="Go to next error (!= line)", action=function()
-  vim.diagnostic.goto_next {
-    severity = vim.diagnostic.severity.ERROR,
-    -- Start searching from next line
-    cursor_position = { --[[ row1 ]] vim.fn.line('.') + 1, --[[ col0 ]] 0 },
-  }
-end}
-local_leader_map{mode="n", key="dk", desc="Go to prev error (!= line)", action=function()
-  vim.diagnostic.goto_prev {
-    severity = vim.diagnostic.severity.ERROR,
-    -- Start searching from previous line
-    cursor_position = { --[[ row1 ]] vim.fn.line('.') - 1, --[[ col0 ]] 0 },
-  }
-end}
+local_leader_map{mode="n", key="dn", action=my_actions.goto_next_diag}
+local_leader_map{mode="n", key="dp", action=my_actions.goto_prev_diag}
+local_leader_map{mode="n", key="dj", action=my_actions.goto_next_diag_diff_line}
+local_leader_map{mode="n", key="dk", action=my_actions.goto_prev_diag_diff_line}
 
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
