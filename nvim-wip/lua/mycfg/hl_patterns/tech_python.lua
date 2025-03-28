7b79478afa4c25193ceb85f2d8aedc721dce5d82
Your Name <you@example.com>
2025-03-28 15:31:19 +0100

    nvim(hl:py): Darker emph values in docs, avoid confusion with real code

--- nvim-wip/lua/mycfg/hl_patterns/tech_python.lua
+++ nvim-wip/lua/mycfg/hl_patterns/tech_python.lua
@@ -11,7 +11,6 @@ patterns.py_docstring_param_name = {
   -- Same as previous, focused on the param name
   pattern = ":param ()[%w_]*():",
   group = _U.define_hl("py_docstring_param_name", {
-    -- bold = true,
     italic = true,
     underdotted = true,
   }),
@@ -26,7 +25,8 @@ patterns.py_docstring_raises = {
 patterns.py_docstring_raises_excs = {
   pattern = ":raises ()[%w_, ]*():",
   group = _U.define_hl("py_docstring_raises_excs", {
-    bold = true,
+    italic = true,
+    underdotted = true,
   }),
 }
 
