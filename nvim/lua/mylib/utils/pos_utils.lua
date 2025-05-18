3e6b3e3f253fd421cb7eefdcd3ef8fbf7ed46cc8
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-05-19 00:42:16 +0200

    nvim(utils): Add search+pos utils to check if pos is on search result

--- nvim/lua/mylib/utils/pos_utils.lua
+++ nvim/lua/mylib/utils/pos_utils.lua
@@ -2,6 +2,9 @@ local U_fmt = require"mylib.utils.fmt_utils"
 local _f = U_fmt.str_space_concat
 local _q = U_fmt.str_simple_quote_surround
 
+---@alias mylib.RangePos0 {start_pos: mylib.Pos0, end_pos: mylib.Pos0}
+
+--- A (0, 0)-indexed normalized position, to simplify reasoning and comparisons
 ---@class mylib.Pos0
 ---@field row integer
 ---@field col integer
@@ -11,22 +14,61 @@ local Pos0 = {}
 ---@param pos0 {row: integer, col: integer}
 ---@return mylib.Pos0
 function Pos0.new(pos0)
-  return setmetatable(pos0, { __index = Pos0 })
+  return setmetatable(pos0, {
+    __index = Pos0,
+    __eq = Pos0.__eq,
+  })
 end
 
---- Create a new Pos0 from cursor position or given vim position str (like "v" or "'k")
----@param kind "pos"|"cursor"
-function Pos0.from_vimpos(kind, ...)
+---@alias mylib.Pos0FromVimposKind
+--- | "pos" A vim position str (like "v", or "'k")
+--- | "pos11" A vim position table (1,1)-indexed
+--- | "cursor" The current cursor position
+
+--- Create a new Pos0 from cursor position, given vim position str, or given position table.
+--- Returns nil if the resulting position is invalid
+---@param kind mylib.Pos0FromVimposKind Kind of vimpos, or how to interpret rest of args
+---@param ... any See `kind`
+---@return mylib.Pos0?
+function Pos0.try_from_vimpos(kind, ...)
+  local pos ---@type mylib.Pos0
   if kind == "cursor" then
     local cursor_pos10 = vim.api.nvim_win_get_cursor(#{...} == 0 and 0 or ...)
-    return Pos0.new{ row = cursor_pos10[1] -1, col = cursor_pos10[2] }
+    pos = Pos0.new{ row = cursor_pos10[1] -1, col = cursor_pos10[2] }
   elseif kind == "pos" then
     ---       <bufnr>  <lnum1>   <col1>  <offset>
     ---@type [integer, integer, integer, integer]
     local posinfo = vim.fn.getpos(...)
-    return Pos0.new{ row = posinfo[2] -1, col = posinfo[3] -1 }
+    pos = Pos0.new{ row = posinfo[2] -1, col = posinfo[3] -1 }
+  elseif kind == "pos11" then
+    -- (1, 1)-indexed position (e.g. from vim fn like `searchpos`)
+    ---@type [integer, integer]
+    local pos11 = ...
+    pos = Pos0.new{ row = pos11[1] -1, col = pos11[2] -1 }
+  else
+    error(_f("Pos0.try_from_vimpos: Unknown kind", _q(kind)))
   end
-  error(_f("Pos0.from_vimpos: Unknown kind", _q(kind)))
+  if not pos:is_valid() then
+    return nil
+  end
+  return pos
+end
+
+--- Same as `Pos0.try_from_vimpos`, but raises if pos obj couldn't be created (aka was invalid)
+---@param kind mylib.Pos0FromVimposKind
+---@return mylib.Pos0
+function Pos0.from_vimpos(kind, ...)
+  local maybe_pos = Pos0.try_from_vimpos(kind, ...)
+  if not maybe_pos then
+    error(_f("Pos0.from_vimpos: Got invalid pos for kind", _q(kind), "args:", ...))
+  end
+  return maybe_pos
+end
+
+--- Whether the position is valid (has positive numbers)
+---@return boolean
+function Pos0:is_valid()
+  return self.row >= 0 and self.col >= 0
 end
 
 --- Create a new Pos0 with applied delta
@@ -37,6 +79,43 @@ function Pos0:with_delta(pos0_delta)
     row = self.row + (pos0_delta.row or 0),
     col = self.col + (pos0_delta.col or 0),
   })
+end
+
+--- Returns a (1,0)-indexed position
+---@return [integer, integer]
+function Pos0:to_1_0_indexed()
+  return {self.row +1, self.col}
+end
+
+--- Check whether position is within an inclusive range (start pos -> end pos)
+---@param range mylib.RangePos0
+---@return boolean
+function Pos0:is_within_incl_range(range)
+  local start_pos, end_pos = range.start_pos, range.end_pos
+  -- Handle single-line matches
+  if start_pos.row == end_pos.row then
+    return (
+      self.row == start_pos.row
+      and start_pos.col <= self.col
+      and self.col <= end_pos.col
+    )
+  end
+
+  -- Handle multi-line matches
+  return (
+    (start_pos.row < self.row or (start_pos.row == self.row and start_pos.col <= self.col))
+    and (self.row < end_pos.row or (self.row == end_pos.row and self.col <= end_pos.col))
+  )
+end
+
+
+---@param other mylib.Pos0
+---@return boolean
+function Pos0:__eq(other)
+  if not type(other) == "table" then
+    return false
+  end
+  return self.row == other.row and self.col == other.col
 end
 
 return {
--- nvim/lua/mylib/utils/search_utils.lua
+++ nvim/lua/mylib/utils/search_utils.lua
@@ -1,4 +1,5 @@
 local U_args = require"mylib.utils.args_utils"
+local Pos0 = require"mylib.utils.pos_utils".Pos0
 
 local U_search = {}
 
@@ -51,6 +52,69 @@ end
 ---@return string
 function U_search.escape_text_for_search(text)
   return vim.fn.escape(text, [[/\]])
+end
+
+--- Find positions of match containing the cursor
+---@param pattern string Search pattern
+---@param pos mylib.Pos0 Position where to search for a match
+---@return mylib.RangePos0?
+function U_search.try_get_match_range_near(pattern, pos)
+  local saved_cursor = vim.api.nvim_win_get_cursor(0) -- (1,0)-indexed API pos
+  local saved_view = vim.fn.winsaveview()
+  local function restore_things()
+    vim.api.nvim_win_set_cursor(0, saved_cursor)
+    vim.fn.winrestview(saved_view)
+  end
+
+  -- Set cursor at pos, as `searchpos` works around cursor pos
+  vim.api.nvim_win_set_cursor(0, {pos.row+1, pos.col})
+
+  -- Search for closest start pos for match
+  ---@type mylib.Pos0
+  local start_pos = (function()
+    -- Try search for match at pos
+    local maybe_match_here = Pos0.try_from_vimpos("pos11", vim.fn.searchpos(pattern, "nc"))
+    if maybe_match_here and maybe_match_here == pos then
+      return maybe_match_here
+    end
+
+    -- Otherwise, try search for last match (pos might be on it, or not)
+    return Pos0.try_from_vimpos("pos11", vim.fn.searchpos(pattern, "ncb"))
+  end)()
+  if not start_pos then
+    -- Didn't find any match, no range
+    restore_things()
+    return nil
+  end
+
+  -- Move cursor to start pos to find the end pos of this match
+  vim.api.nvim_win_set_cursor(0, start_pos:to_1_0_indexed())
+  local end_pos = Pos0.from_vimpos("pos11", vim.fn.searchpos(pattern, "nce"))
+
+  restore_things()
+  return { start_pos = start_pos, end_pos = end_pos }
+end
+
+--- Check if cursor is on a search match (supports multiline)
+---@param pos mylib.Pos0 Position to check
+---@return boolean
+function U_search.is_pos_on_search_match(pos)
+  if vim.fn.searchcount().total == 0 then
+    -- Early return, there are zero match in the file
+    return false
+  end
+
+  local pattern = vim.fn.getreg("/")
+  -- note: missing pattern already handled by searchcount's early return above
+
+  -- Try get near match range
+  local range = U_search.try_get_match_range_near(pattern, pos)
+  if not range then
+    return false
+  end
+
+  -- Check if pos is within match range
+  return pos:is_within_incl_range(range)
 end
 
 return U_search
