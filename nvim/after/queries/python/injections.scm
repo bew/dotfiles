6dfa20c62f3e458f6b14c550aa0943a1cf570064
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2025-09-08 14:04:38 +0200

    nvim(ts:py): Add SQL lang injection in `cur.execute("…")` like code

---
+++ nvim/after/queries/python/injections.scm
@@ -0,0 +1,18 @@
+;; extends
+
+; For sql function call like `cur.execute("SELECT ...")`
+(call
+  function: (attribute
+    object: (identifier) @_obj
+    attribute: (identifier) @_attr
+  )
+  arguments: (argument_list
+    . ; the string must be the first arg!
+    (string
+      (string_content) @injection.content
+    )
+  )
+  (#any-of? @_obj "cur" "cursor")
+  (#eq? @_attr "execute")
+  (#set! injection.language "sql")
+)
