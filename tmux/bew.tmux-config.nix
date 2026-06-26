b40b4cc7dda7440fbb6ac07a59b2a827a6a0a224
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2026-06-26 10:06:50 +0200

    tmux: Add first plugin! tmux-resurrect to save/restore win/pane layout 🚀

--- tmux/bew.tmux-config.nix
+++ tmux/bew.tmux-config.nix
@@ -11,6 +11,19 @@ in {
 
   ID = "bew";
 
+  env.TMUX_PLUGIN_RESURRECT_PATH.set = let
+    pluginDrv = pkgs.tmuxPlugins.resurrect.overrideAttrs (finalAttrs: {
+      patches = [
+        # Patch to add '@resurrect-hook-pre-save-all' hook exec
+        # PR: https://github.com/tmux-plugins/tmux-resurrect/pull/574
+        (pkgs.fetchpatch {
+          url = "https://github.com/bew/tmux-resurrect/commit/258b32f4ed63b36043c3a63f69c427ae58b1b078.patch";
+          hash = "sha256-ZOiy2hdyrxaXswaX/98fbt/fN4LSw8hp1ScJ8DPYFNA=";
+        })
+      ];
+    });
+  in "${pluginDrv}/share/tmux-plugins/resurrect";
+
   outputs.editable-cfgDir = cfg.lib.mkLink ./.;
 
   # Only depend on conf files and scripts (skip Nix files to avoid useless rebuilds)
