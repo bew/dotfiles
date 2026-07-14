6f8766a8f645d58e1a74c2bc8e95cf7f5e1d7330
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2026-07-14 03:56:02 +0200

    nvim(ft:rs): Fix doc comment formatting!

--- nvim/ftplugin/rust.lua
+++ nvim/ftplugin/rust.lua
@@ -1,3 +1,7 @@
 -- Prevent the builtin ftplugin from running and defining its own commands
 -- (want to use rustaceanvim!)
 vim.b.did_ftplugin = 1
+
+-- Allow both `///` & `//` (order matters) to be considered as a comment leader
+-- (e.g. on gq or 'Enter' from a doc line when formatoptions includes 'r')
+vim.o.comments = ":///,://"
