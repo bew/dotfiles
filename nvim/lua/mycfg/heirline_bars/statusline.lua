b69eb606c60dc0aed3d00d951628fe95e39c2732
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2026-09-18 01:37:29 +0200

    nvim: misc

--- nvim/lua/mycfg/heirline_bars/statusline.lua
+++ nvim/lua/mycfg/heirline_bars/statusline.lua
@@ -26,22 +26,31 @@ local GeneralPurposeStatusline = {
     end,
   },
   C.lsp_ts_diags.Diagnostics,
+
+  -- e.g. `5 cursors`
+  -- e.g. `2 cursors (+5 off)`
+  -- TODO: extract into a dedicated heirline component!
   {
     condition = function()
-      local mc = require"multicursor-nvim"
-      return hline_conditions.is_active() and mc.hasCursors()
+      local mc = U.is_module_available"multicursor-nvim" and require"multicursor-nvim"
+      return hline_conditions.is_active() and mc and mc.hasCursors()
     end,
+
     _,
-    -- 5 cursors
-    -- 2 cursors (+5 off)
     {
+      -- WARN: this is not called enough times, especially not called when I clear all cursors ><
+      -- OPENED: https://github.com/jake-stewart/multicursor.nvim/issues/155
+      -- I tried to impl an autocmd based on SafeState as suggested
+      -- => did NOT work as expected, in weird ways...
+
       provider = function()
         local mc = require"multicursor-nvim"
-        local num_disabled = mc.numDisabledCursors()
-        local num_enabled = mc.numEnabledCursors()
-        local ret = _f(num_enabled, "cursors")
-        if num_disabled > 0 then
-          ret = ret .. U.fmt.str_concat(" (+", num_disabled, " off)")
+        local enabled_count = mc.numEnabledCursors()
+        local disabled_count = mc.numDisabledCursors()
+        vim.notify("in component provider! enabled? " .. tostring(disabled_count > 0) .. " enabled:"..tostring(enabled_count) .. " disabled:"..tostring(disabled_count))
+        local ret = _f(enabled_count, "cursors")
+        if disabled_count > 0 then
+          ret = ret .. U.fmt.str_concat(" (+", disabled_count, " off)")
         end
         return ret
       end,
@@ -54,7 +63,9 @@ local GeneralPurposeStatusline = {
       end,
     },
   },
+
   __WIDE_SPACE__,
+
   external_components.lsp({ lsp_client_names = false }), -- LSP progress messages
   {
     condition = C.lsp_ts_diags.LspActive.condition,
