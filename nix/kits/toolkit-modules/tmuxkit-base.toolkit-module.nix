a48ce08b0bb404eafbebd903f6f493ab0f6d545b
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2026-09-23 15:48:04 +0200

    nix(tmuxkit): Unify tmux wrapper across standalone/Home Manager
    Both the standalone package and the Home Manager install now build the
    tmux binary through a shared `makeTmuxWrapperPkg` helper.
    Home Manager installs therefore carry the `cfg.env` vars, the
    `TERMINFO_DIRS` prefix, and `TMUX_CONFIG_ENTRYPOINT` like standalone.
    
    The config entrypoint flag stays standalone-only, passed through the
    helper's `extraWrapperParams`.

--- nix/kits/toolkit-modules/tmuxkit-base.toolkit-module.nix
+++ nix/kits/toolkit-modules/tmuxkit-base.toolkit-module.nix
@@ -6,6 +6,43 @@ let
   ty = lib.types;
   cfg = config;
   outs = config.outputs;
+
+  makeTmuxWrapperPkg = { extraWrapperParams ? "" }:
+    mypkglib.replaceBinsInPkg (let
+      binName = "tmux"; # for now, until I fix my `tx` alias, when used in `nix run dots`… 👀
+      # binName = "tmux-${config.ID}";
+    in {
+      name = binName;
+      copyFromPkg = config.package;
+      nativeBuildInputs = [ pkgs.makeWrapper ];
+      meta.mainProgram = binName;
+      postBuild = /* sh */ ''
+        wrapperArgs=(
+          ${extraWrapperParams}
+          # Send env var for config reloads
+          --set TMUX_CONFIG_ENTRYPOINT ${outs.cfgEntrypoint}
+          # Inject other env vars
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: valSpec:
+            if valSpec.set != null then
+              "--set ${name} ${lib.escapeShellArg valSpec.set}"
+            else if valSpec.set-default != null then
+              "--set-default ${name} ${lib.escapeShellArg valSpec.set-default}"
+            else
+              throw "tmux-toolkit: env.${name}: action not supported ☹️"
+          ) cfg.env)}
+          --prefix TERMINFO_DIRS : ${config.package.terminfo}/share/terminfo
+        )
+        makeWrapper ${cfg.package}/bin/tmux $out/bin/${binName} "''${wrapperArgs[@]}"
+
+        # Add useful paths in the package (for easy introspection)
+        mkdir -p $out/useful-paths
+        ln -s ${outs.cfgDir} $out/useful-paths/config
+      '';
+      # NOTE: we need to prepend terminfo to ensure the programs running in tmux find the correct
+      #   terminfo database for the running tmux. (and not an eventual outdated system version)
+      # -> This is necessary on MacOS to make colored underline work in Neovim under tmux.
+      #    ref: https://github.com/neovim/neovim/issues/29649
+    });
 in {
   _class = "tool.tmux"; # type of nix module
 
@@ -51,46 +88,16 @@ in {
     ];
     outputs.cfgEntrypoint = "${outs.cfgDir}/tmux.conf";
 
-    outputs.toolPkg.standalone = mypkglib.replaceBinsInPkg (let
-      binName = "tmux"; # for now, until I fix my `tx` alias, when used in `nix run dots`… 👀
-      # binName = "tmux-${config.ID}";
-    in {
-      name = binName;
-      copyFromPkg = config.package;
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      meta.mainProgram = binName;
-      postBuild = /* sh */ ''
-        wrapperArgs=(
-          # Start tmux with custom config entrypoint
-          --add-flags "-f ${outs.cfgEntrypoint}"
-          # Send env var for config reloads
-          --set TMUX_CONFIG_ENTRYPOINT ${outs.cfgEntrypoint}
-          # Inject other env vars
-          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: valSpec:
-            if valSpec.set != null then
-              "--set ${name} ${lib.escapeShellArg valSpec.set}"
-            else if valSpec.set-default != null then
-              "--set-default ${name} ${lib.escapeShellArg valSpec.set-default}"
-            else
-              throw "tmux-toolkit: env.${name}: action not supported ☹️"
-          ) cfg.env)}
-          --prefix TERMINFO_DIRS : ${config.package.terminfo}/share/terminfo
-        )
-        makeWrapper ${cfg.package}/bin/tmux $out/bin/${binName} "''${wrapperArgs[@]}"
-
-        # Add useful paths in the package (for easy introspection)
-        mkdir -p $out/useful-paths
-        ln -s ${outs.cfgDir} $out/useful-paths/config
+    outputs.toolPkg.standalone = makeTmuxWrapperPkg {
+      extraWrapperParams = ''
+        # Start tmux with custom config entrypoint
+        --add-flags "-f ${outs.cfgEntrypoint}"
       '';
-      # NOTE: we need to prepend terminfo to ensure the programs running in tmux find the correct
-      #   terminfo database for the running tmux. (and not an eventual outdated system version)
-      # -> This is necessary on MacOS to make colored underline work in Neovim under tmux.
-      #    ref: https://github.com/neovim/neovim/issues/29649
-    });
+    };
 
     outputs.homeModules.withDefaults = {
       xdg.configFile."tmux".source = outs.cfgDir;
-      home.packages = [ config.package ];
+      home.packages = [ (makeTmuxWrapperPkg {}) ];
     };
   };
 }
